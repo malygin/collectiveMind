371f244a7dd9e2e011382a1e07d027dafd92e550
malygin <anmalygin@gmail.com>
2012-07-20 16:23:14 +0400

    add comment edit and delete, HAML template

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
   attr_accessible :text, :title
 
   validates :title, :presence => true, :length => { :minimum =>5 }
-  has_many :comments
+  has_many :comments, :dependent => :destroy
 end
