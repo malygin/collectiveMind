085502491fdb84ab1293f51e8c7cc73717c8d561
malygin <anmalygin@gmail.com>
2012-09-11 19:30:37 +0400

    add surveyor complete

--- db/migrate/20120907202727_add_content_text_to_frustrations.rb
+++
@@ -1,6 +0,0 @@
-class AddContentTextToFrustrations < ActiveRecord::Migration
-  def change
-  	add_column :frustrations, :content_text, :string
-  	add_column :frustrations, :old_content_text, :string
-  end
-end
