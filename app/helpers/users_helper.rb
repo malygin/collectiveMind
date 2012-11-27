ed9b2a2f4b07d43a8579cc46a327312d09b8b6ae
malygin <anmalygin@gmail.com>
2012-11-27 17:29:29 +0400

    add avatar of student

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -14,8 +14,11 @@ module UsersHelper
 		# end
 		# gravatar_image_tag(user.email.downcase, :alt => user.name, :align => 'left',
 		# 	:class =>'avatar', :gravatar => options)
-
-		image_tag('http://oecdo.sgu.ru/ImageServlet?user='+user.login, :class =>'avatar')
+		if user.login.nil?
+			image_tag('http://oec-static.main.sgu.ru/storage/oec-j2ee6/ROOT/userFiles/avatars/defaultAvatarSmall.jpg', :class =>'avatar')
+		else
+			image_tag('http://oecdo.sgu.ru/ImageServlet?user='+user.login, :class =>'avatar')
+		end
 	end
 
 	def available_form_adding_frustration?(user)
