8a7f0cf237662db8e6b807f46b47318a325ac696
malygin <anmalygin@gmail.com>
2012-11-29 15:21:50 +0400

    wrong url to help image and new video to help

--- config/application.rb
+++ config/application.rb
@@ -53,11 +53,11 @@ module CollectiveMind
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
-         config.time_zone = 'Moscow'# +0400 
-         config.active_record.default_timezone = 'Moscow'# +0400 
+         # config.time_zone = 'Moscow'# +0400 
+         # config.active_record.default_timezone = 'Moscow'# +0400 
     # Enable the asset pipeline
     config.assets.enabled = true
-
+    config.assets.initialize_on_precompile = false
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.generators do |g|
