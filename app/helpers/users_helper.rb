277a046f68d3e1245e56ee853bc3326dfd697995
malygin <anmalygin@gmail.com>
2012-12-08 23:58:10 +0400

    bug with nil user for post

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -14,7 +14,9 @@ module UsersHelper
 		# end
 		# gravatar_image_tag(user.email.downcase, :alt => user.name, :align => 'left',
 		# 	:class =>'avatar', :gravatar => options)
-		if user.login.nil?
+		if user.nil?
+			""
+		elsif user.login.nil?
 			image_tag('http://oec-static.main.sgu.ru/storage/oec-j2ee6/ROOT/userFiles/avatars/defaultAvatarSmall.jpg', :class =>'avatar')
 		else
 			image_tag('http://oecdo.sgu.ru/ImageServlet?user='+user.login, :class =>'avatar')
