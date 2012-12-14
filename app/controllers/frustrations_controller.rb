2ecf18f1c24cae2a0ab3f76047ad465e44b4004e
malygin <anmalygin@gmail.com>
2012-12-14 12:26:49 +0400

    html safe for comment and score for forecast

--- app/controllers/frustrations_controller.rb
+++ app/controllers/frustrations_controller.rb
@@ -46,7 +46,7 @@ class FrustrationsController < ApplicationController
 				dic = {}
 				score = 0
 				dic[f.frustration] = f.order
-				if f.frustration in fr_with_orders.keys
+				if fr_with_orders.keys.includes?  f.frustration 
 					score +=5
 				end
 				if fr_with_orders[f.frustration] == f.order
@@ -54,7 +54,7 @@ class FrustrationsController < ApplicationController
 				end	
 				@fres[f.user]=[score, dic]
 			else
-				if f.frustration in fr_with_orders.keys
+				if fr_with_orders.keys.includes?  f.frustration 
 					@fres[f.user][0] += 5
 				end
 				if fr_with_orders[f.frustration] == f.order
