de679fc0e9824f6064164d3887a3f4dc9fc803ca
malygin <anmalygin@gmail.com>
2013-01-30 17:28:13 +0400

    add new test

--- app/models/test.rb
+++ app/models/test.rb
@@ -7,5 +7,16 @@ class Test < ActiveRecord::Base
   def ordering_questions
   	self.test_questions.find(:all, :order => "order_question ASC")
   end
+  def avialable?
+  	if test_attempts.empty?
+  		return true
+  	end
+  	test_attempts.each do |a|
+  		if a.user == curent_user
+  			return false
+  		end
+  	end
+  	true
+  end
 
 end
