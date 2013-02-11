80d2c9714e7161816e3498d03cff9e98f6902074
malygin <anmalygin@gmail.com>
2013-02-11 16:16:00 +0400

    add wiselink

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,6 @@ config.static_cache_control = "public, max-age=2592000"
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
-  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
    config.assets.precompile += %w( google.js )
 
   # Disable delivery errors, bad email addresses will be ignored
