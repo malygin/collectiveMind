6eb09a556657872e655cf3c9a0f80d24ccb0fc19
malygin <anmalygin@gmail.com>
2013-02-19 14:33:28 +0400

    bug with main page rout and projects controller

--- config/routes.rb
+++ config/routes.rb
@@ -250,7 +250,7 @@ end
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'core_projects#index'
+  root :to => 'core/projects#index'
 
   # See how all your routes lay out with "rake routes"
 
