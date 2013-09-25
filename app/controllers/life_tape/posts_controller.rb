c02b88aef4ae266af25ac24c06c45ac4a20d572f
andrey malygin <anmalygin@gmail.com>
2013-09-25 13:33:43 +0400

    voting information for boss

--- app/controllers/life_tape/posts_controller.rb
+++ app/controllers/life_tape/posts_controller.rb
@@ -56,7 +56,7 @@ end
       @all_people = @project.users.size
 
       @voted_people = ActiveRecord::Base.connection.execute("select count(*) as r from (select distinct v.user_id from life_tape_voitings v  left join   discontent_aspects asp on (v.discontent_aspect_id = asp.id) where asp.project_id = #{@project.id}) as dm").first["r"]
-      @votes = ActiveRecord::Base.connection.execute("select count(*) as r from (select  v.user_id from life_tape_voitings v  left join   discontent_aspects asp on (v.discontent_aspect_id = asp.id) where asp.project_id = #{@project.id}) as dm").first["r"]
+      @votes = ActiveRecord::Base.connection.execute("select count(*) as r from (select  v.user_id from life_tape_voitings v  left join   discontent_aspects asp on (v.discontent_aspect_id = asp.id) where asp.project_id = #{@project.id}) as dm").first["r"].to_i
     end
   end
 
