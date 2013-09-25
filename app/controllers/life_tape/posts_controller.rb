3c073423951141a708535c62b93b1e91baf3c320
andrey malygin <anmalygin@gmail.com>
2013-09-25 13:24:50 +0400

    voting information for boss

--- app/controllers/life_tape/posts_controller.rb
+++ app/controllers/life_tape/posts_controller.rb
@@ -48,9 +48,16 @@ end
   end
   def vote_list
     @posts = voting_model.where(:project_id => @project)
+
     @number_v = @project.stage1.to_i - current_user.voted_aspects.size
     @path_for_voting = "/project/#{@project.id}/life_tape/"
-     @votes = @project.stage1
+    @votes = @project.stage1
+    if boss?
+      @all_people = @project.users.size
+
+      @voted_people = ActiveRecord::Base.connection.execute("select count(*) as r from (select distinct v.user_id from life_tape_voitings v  left join   discontent_aspects asp on (v.discontent_aspect_id = asp.id) where asp.project_id = #{@project.id}) as dm").first["r"]
+      @votes = ActiveRecord::Base.connection.execute("select count(*) as r from (select  v.user_id from life_tape_voitings v  left join   discontent_aspects asp on (v.discontent_aspect_id = asp.id) where asp.project_id = #{@project.id}) as dm").first["r"]
+    end
   end
 
 end
--- app/models/life_tape/voiting.rb
+++ app/models/life_tape/voiting.rb
@@ -1,5 +1,5 @@
 class LifeTape::Voiting < ActiveRecord::Base
     attr_accessible  :user
     belongs_to :user
-	belongs_to :discontent_aspect
+	  belongs_to :discontent_aspect
 end
