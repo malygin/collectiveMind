e325fb18b9fa3518503107fef72b1066969010d1
andrey malygin <anmalygin@gmail.com>
2013-09-27 13:57:31 +0400

    news for discontent and more info for posts

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -11,7 +11,7 @@ class Journal < ActiveRecord::Base
            'life_tape_post_save', 'life_tape_comment_save', 'concept_post_update',
            'question_post_save','question_comment_save', 'expert_news_comment_save','expert_news_post_save','estimate_post_save',
            'estimate_post_update','estimate_comment_save', 'answer_save', 'estimate_post_rejection', 'estimate_post_acceptance']
-  def self.events_for_user_feed(project_id, lim = 9)
+  def self.events_for_user_feed(project_id, lim = 20)
 
 		Journal.where(" project_id = (?) AND type_event IN (?)",project_id, @types).limit(lim).order('created_at DESC')
   end
