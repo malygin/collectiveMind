81acd1afadc32847e00640ebdd1191bf659b2c3a
andrey malygin <anmalygin@gmail.com>
2014-02-04 23:56:06 +0400

    animated form for new life tape

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,21 @@
+//= require jquery
+//= require jquery_ujs
+//= require_tree
+//= require jquery.ui.all
+//= require highcharts
+//= require highcharts/highcharts-more
+//= require selectize
+//= require twitter/bootstrap
+
+
+
+
+function get_life_tape_form(){
+    $('#new_life_tape').css('display','block');
+    $("#new_life_tape").animate({
+        height: 100
+    }, "normal");
+
+}
+
+
