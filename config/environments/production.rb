6581de7e545ef86df0f194c953de99af113fbc4d
andrey malygin <anmalygin@gmail.com>
2014-02-25 01:12:17 +0400

    precompile for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ config.static_cache_control = 'public, max-age=2592000'
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
