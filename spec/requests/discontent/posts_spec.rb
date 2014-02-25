dacfd47b5b573c8b42c68a500b49042a56cad950
andrey malygin <anmalygin@gmail.com>
2014-02-26 01:34:34 +0400

    add spec for discontent

---
+++ spec/requests/discontent/posts_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+describe 'Discontents ' do
+  subject { page }
+  before :all do
+
+  end
+  context  'user sign in ' do
+
+    context 'visit post lists' do
+      xit 'vote for discontent'
+    end
+
+  end
+
+end
--- spec/requests/life_tape/posts_spec.rb
+++ spec/requests/life_tape/posts_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
-describe 'Life Tape Posts' do
+describe 'Life Tape ' do
   subject { page }
 
   before :all do
