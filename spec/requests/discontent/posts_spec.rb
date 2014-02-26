43e7ee549230bc5d8e5832a222b2c04c1703f51c
andrey malygin <anmalygin@gmail.com>
2014-02-26 13:21:13 +0400

    template spec discontent

--- spec/requests/discontent/posts_spec.rb
+++ spec/requests/discontent/posts_spec.rb
@@ -7,7 +7,27 @@ describe 'Discontents ' do
   context  'user sign in ' do
 
     context 'visit post lists' do
-      xit 'vote for discontent'
+      xit 'show mini-help'
+      xit 'vote for discontent and against'
+      xit 'filter by aspects'
+      xit 'filter by popularity'
+    end
+
+    context 'add post' do
+      xit 'user can add only what field'
+      xit 'auto complete for where and when'
+      xit 'show similar posts for adding '
+    end
+
+    context 'edit post' do
+      xit 'edit hm'
+    end
+
+    context 'show one post' do
+      xit 'vote for post'
+      xit 'add comment'
+      xit 'vote for comment'
+      xit 'warning admin about post'
     end
 
   end
