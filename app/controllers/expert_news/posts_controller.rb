16792ef48fb59ae67e1055c342f8f1e361bc18a4
andrey malygin <anmalygin@gmail.com>
2014-03-08 01:20:35 +0400

    rebuild extra news

--- app/controllers/expert_news/posts_controller.rb
+++ app/controllers/expert_news/posts_controller.rb
@@ -10,5 +10,10 @@ class ExpertNews::PostsController  < PostsController
   def comment_model
     ExpertNews::Comment
   end
-
+  def prepare_data
+    @project = Core::Project.find(params[:project])
+    add_breadcrumb I18n.t('menu.news'), expert_news_posts_path(@project)
+    @journals = Journal.events_for_user_feed @project.id
+    @news = ExpertNews::Post.where(:project_id => @project).first
+  end
 end
