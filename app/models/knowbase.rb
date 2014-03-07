cd695a3e604c61c61ef3200f5f5866adab4b2ee1
andrey malygin <anmalygin@gmail.com>
2014-03-07 19:53:04 +0400

    knowledge base

---
+++ app/models/knowbase.rb
@@ -0,0 +1,5 @@
+module Knowbase
+  def self.table_name_prefix
+    'knowbase_'
+  end
+end
---
+++ app/models/knowbase/post.rb
@@ -0,0 +1,6 @@
+class Knowbase::Post < ActiveRecord::Base
+  attr_accessible :content, :title, :stage
+
+  belongs_to :project, :class_name => "Core::Project"
+
+end
