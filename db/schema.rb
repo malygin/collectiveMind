488a05b7f0c5efc523dfd713e340c96d73072c3e
andrey malygin <anmalygin@gmail.com>
2014-03-12 01:03:13 +0400

    new form for post aspect

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140307145323) do
+ActiveRecord::Schema.define(:version => 20140311205303) do
 
   create_table "answers", :force => true do |t|
     t.string   "text"
@@ -98,6 +98,8 @@ ActiveRecord::Schema.define(:version => 20140307145323) do
     t.string   "content"
     t.text     "reality"
     t.text     "problems"
+    t.text     "positive_r"
+    t.text     "negative_r"
   end
 
   create_table "concept_post_notes", :force => true do |t|
