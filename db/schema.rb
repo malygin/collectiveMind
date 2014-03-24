cfb7940c534283c510e7ea2bcc8aad518fd2fc20
andrey malygin <anmalygin@gmail.com>
2014-03-24 22:51:46 +0400

    migration for content and name post_aspects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140319092728) do
+ActiveRecord::Schema.define(:version => 20140324155804) do
 
   create_table "answers", :force => true do |t|
     t.string   "text"
@@ -90,13 +90,13 @@ ActiveRecord::Schema.define(:version => 20140319092728) do
   create_table "concept_post_aspects", :force => true do |t|
     t.integer  "discontent_aspect_id"
     t.integer  "concept_post_id"
-    t.datetime "created_at",           :null => false
-    t.datetime "updated_at",           :null => false
+    t.datetime "created_at",                          :null => false
+    t.datetime "updated_at",                          :null => false
     t.text     "positive"
     t.text     "negative"
     t.text     "control"
-    t.string   "name"
-    t.string   "content"
+    t.text     "name",                 :limit => 255
+    t.text     "content",              :limit => 255
     t.text     "reality"
     t.text     "problems"
     t.text     "positive_r"
@@ -421,12 +421,6 @@ ActiveRecord::Schema.define(:version => 20140319092728) do
     t.integer  "ozf4"
     t.integer  "ozs4"
     t.integer  "on4"
-    t.integer  "nep1"
-    t.integer  "nep2"
-    t.integer  "nep3"
-    t.integer  "nep4"
-    t.text     "nep"
-    t.integer  "all_grade"
     t.boolean  "first_stage"
     t.integer  "plan_post_first_cond_id"
   end
@@ -586,10 +580,12 @@ ActiveRecord::Schema.define(:version => 20140319092728) do
     t.string   "comment_admin"
     t.boolean  "trash",                  :default => false
     t.integer  "frustration_comment_id"
+    t.integer  "useful_frustration_id"
   end
 
   add_index "frustration_comments", ["created_at"], :name => "index_frustration_comments_on_created_at"
   add_index "frustration_comments", ["frustration_id"], :name => "index_frustration_comments_on_frustration_id"
+  add_index "frustration_comments", ["useful_frustration_id"], :name => "index_frustration_comments_on_useful_frustration_id"
   add_index "frustration_comments", ["user_id"], :name => "index_frustration_comments_on_user_id"
 
   create_table "frustration_essays", :force => true do |t|
@@ -802,10 +798,10 @@ ActiveRecord::Schema.define(:version => 20140319092728) do
     t.text     "problems"
     t.text     "reality"
     t.integer  "first_stage"
-    t.string   "name"
-    t.string   "content"
-    t.datetime "created_at",             :null => false
-    t.datetime "updated_at",             :null => false
+    t.text     "name",                   :limit => 255
+    t.text     "content",                :limit => 255
+    t.datetime "created_at",                            :null => false
+    t.datetime "updated_at",                            :null => false
     t.integer  "concept_post_aspect_id"
     t.text     "positive_r"
     t.text     "negative_r"
@@ -897,9 +893,6 @@ ActiveRecord::Schema.define(:version => 20140319092728) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
-
-# Could not dump table "projects" because of following StandardError
-#   Unknown type 'serial' for column 'id'
 
   create_table "question_comment_votings", :force => true do |t|
     t.integer  "user_id"
