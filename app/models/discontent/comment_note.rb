b0d06be4b71fb40dbe4e9531486dd9f24ef9461f
Andrey Malygin <anmalygin@gmail.com>
2014-04-10 14:00:27 +0400

    merge conflict

--- app/models/discontent/comment_note.rb
+++ app/models/discontent/comment_note.rb
@@ -1,7 +1,15 @@
 class Discontent::CommentNote < ActiveRecord::Base
+<<<<<<< HEAD
   attr_accessible :content, :post_id, :user_id, :type_field, :status
   belongs_to :user, :class_name => "User"
   belongs_to :post, :class_name => "Discontent::Post"
 
   scope :by_type,   ->(type){where(type_field: type)}
+=======
+  # attr_accessible :content, :post_id, :type, :user_id
+  # include BaseComment
+  attr_accessible :content, :post_id, :type_field, :user_id
+  belongs_to :user, :class_name => "User"
+  belongs_to :post, :class_name => "Discontent::Post"
+>>>>>>> merge conflict
 end
