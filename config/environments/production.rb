0245c43a8991b4467c1f262920de4861a95adb7f
Andrey Malygin <anmalygin@gmail.com>
2014-05-07 16:01:43 +0400

    remove dalli from production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,26 +8,26 @@ CollectiveMind::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-#  # Disable Rails's static asset server (Apache or nginx will already do this)
-#  config.serve_static_assets = false
-#config.cache_store = :dalli_store
-#config.action_dispatch.rack_cache = {
-#  :metastore    => Dalli::Client.new,
-#  :entitystore  => 'file:tmp/cache/rack/body',
-#  :allow_reload => false
-#}
-config.serve_static_assets = true
+  #  # Disable Rails's static asset server (Apache or nginx will already do this)
+  #  config.serve_static_assets = false
+  #config.cache_store = :dalli_store
+  #config.action_dispatch.rack_cache = {
+  #  :metastore    => Dalli::Client.new,
+  #  :entitystore  => 'file:tmp/cache/rack/body',
+  #  :allow_reload => false
+  #}
+  config.serve_static_assets = true
 
 
-  config.cache_store = :dalli_store
-
-  client = Dalli::Client.new(ENV["MEMCACHIER_SERVERS"], :value_max_bytes => 10485760,:expires_in => 86400) # 1 day
-
-  # Configure rack-cache for using memcachier
-  config.action_dispatch.rack_cache = {
-      :metastore    => client,
-      :entitystore  => client
-  }
+  #config.cache_store = :dalli_store
+  #
+  #client = Dalli::Client.new(ENV["MEMCACHIER_SERVERS"], :value_max_bytes => 10485760,:expires_in => 86400) # 1 day
+  #
+  ## Configure rack-cache for using memcachier
+  #config.action_dispatch.rack_cache = {
+  #    :metastore    => client,
+  #    :entitystore  => client
+  #}
 
 
 config.static_cache_control = 'public, max-age=2592000'
