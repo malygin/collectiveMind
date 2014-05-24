e6e36ed555924d3eebb2e4b26b8377e37d6724aa
irivertime <irivertime@mail.ru>
2014-05-25 01:47:42 +0400

    fix

--- app/models/life_tape/post.rb
+++ app/models/life_tape/post.rb
@@ -8,7 +8,7 @@ class LifeTape::Post < ActiveRecord::Base
   has_many :post_discussion_users, :through => :life_tape_post_discussions, :source => :user, :class_name => 'User'
   scope :by_discussions, ->(posts) { where("life_tape_posts.id NOT IN (#{posts.join(", ")})") unless posts.empty? }
 
-  #belongs_to :aspect, :class_name =>'Discontent::Aspect', :foreign_key =>  'aspect_id'
+  belongs_to :aspect, :class_name =>'Discontent::Aspect', :foreign_key =>  'aspect_id'
   has_and_belongs_to_many :discontent_aspects, :class_name => 'Discontent::Aspect',  join_table: 'discontent_aspects_life_tape_posts', foreign_key: 'life_tape_post_id', association_foreign_key: 'discontent_aspect_id'
 
 
