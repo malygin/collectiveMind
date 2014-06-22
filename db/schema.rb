292edf5ace36704eedb0036d95750c6e7c203c0b
Andrey Malygin <anmalygin@gmail.com>
2014-06-22 22:48:52 +0400

    show right aspects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140621210443) do
+ActiveRecord::Schema.define(:version => 20140621214250) do
 
   create_table "answers", :force => true do |t|
     t.string   "text"
