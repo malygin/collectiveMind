73d768e2245e0075b814d6f75554e808fa6afe5a
Andrey Malygin <anmalygin@gmail.com>
2014-07-03 01:31:09 +0400

    fix estimate  form

--- db/schema.rb
+++ db/schema.rb
@@ -533,12 +533,6 @@ ActiveRecord::Schema.define(:version => 20140701230004) do
     t.float    "ozf4"
     t.float    "ozs4"
     t.float    "on4"
-    t.integer  "nep1"
-    t.integer  "nep2"
-    t.integer  "nep3"
-    t.integer  "nep4"
-    t.text     "nep"
-    t.integer  "all_grade"
     t.boolean  "first_stage"
     t.integer  "plan_post_first_cond_id"
   end
@@ -698,10 +692,12 @@ ActiveRecord::Schema.define(:version => 20140701230004) do
     t.string   "comment_admin"
     t.boolean  "trash",                  :default => false
     t.integer  "frustration_comment_id"
+    t.integer  "useful_frustration_id"
   end
 
   add_index "frustration_comments", ["created_at"], :name => "index_frustration_comments_on_created_at"
   add_index "frustration_comments", ["frustration_id"], :name => "index_frustration_comments_on_frustration_id"
+  add_index "frustration_comments", ["useful_frustration_id"], :name => "index_frustration_comments_on_useful_frustration_id"
   add_index "frustration_comments", ["user_id"], :name => "index_frustration_comments_on_user_id"
 
   create_table "frustration_essays", :force => true do |t|
@@ -1087,9 +1083,6 @@ ActiveRecord::Schema.define(:version => 20140701230004) do
     t.datetime "updated_at", :null => false
   end
 
-# Could not dump table "projects" because of following StandardError
-#   Unknown type 'serial' for column 'id'
-
   create_table "question_comment_votings", :force => true do |t|
     t.integer  "user_id"
     t.integer  "comment_id"
@@ -1163,9 +1156,9 @@ ActiveRecord::Schema.define(:version => 20140701230004) do
     t.integer  "user_id"
     t.string   "check_field"
     t.boolean  "status"
+    t.integer  "project_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
-    t.integer  "project_id"
     t.string   "value"
   end
 
