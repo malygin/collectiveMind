27d20b75d33205d87d27b937b0926f1429f05dd1
irivertime <irivertime@mail.ru>
2014-07-08 21:43:04 +0400

    add migration for new role

---
+++ db/migrate/20140708172919_remove_columns_to_user.rb
@@ -0,0 +1,15 @@
+class RemoveColumnsToUser < ActiveRecord::Migration
+  # def up
+  #   remove_column :users, :admin
+  #   remove_column :users, :expert
+  #   remove_column :users, :jury
+  #   remove_column :users, :user_type
+  # end
+  #
+  # def down
+  #   add_column :users, :admin, :boolean, :default => false
+  #   add_column :users, :expert, :boolean, :default => false
+  #   add_column :users, :jury, :boolean, :default => false
+  #   add_column :users, :user_type, :string
+  # end
+end
