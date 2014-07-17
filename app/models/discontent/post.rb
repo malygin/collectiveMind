a8dcd06d9e51168781e80138388bc17c2df142b8
irivertime <irivertime@mail.ru>
2014-07-17 15:06:25 +0400

    fix union

--- app/models/discontent/post.rb
+++ app/models/discontent/post.rb
@@ -56,6 +56,7 @@ class Discontent::Post < ActiveRecord::Base
 
   scope :not_view, ->(posts) { where("discontent_posts.id NOT IN (#{posts.join(", ")})") unless posts.empty? }
   scope :created_order, order("created_at DESC")
+  scope :updated_order, order("updated_at DESC")
 
   #scope :uniquely_whend, :select => 'distinct whend'
   #scope :uniquely_whered, :select => 'distinct whered'
