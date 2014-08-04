4b6b9ba1aa307882ce97a7ce01a21465921b55b5
irivertime <irivertime@mail.ru>
2014-08-04 18:21:32 +0400

    fix discontent votings

--- app/models/discontent/voting.rb
+++ app/models/discontent/voting.rb
@@ -6,4 +6,6 @@ class Discontent::Voting < ActiveRecord::Base
     scope :by_positive, ->{where(against: 't')}
     scope :by_negative, ->{where(against: 'f')}
     scope :uniq_user,   ->{select('distinct user_id')}
+
+    scope :by_posts_vote, ->(posts){where("discontent_votings.discontent_post_id IN (#{posts})") unless posts.empty?}
 end
