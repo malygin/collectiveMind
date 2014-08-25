05b3982319a99db42e586fd589167bbcd9d9625c
irivertime <irivertime@mail.ru>
2014-08-25 21:36:57 +0400

    fix tooltip and discontent

--- app/helpers/discontent/posts_helper.rb
+++ app/helpers/discontent/posts_helper.rb
@@ -136,10 +136,7 @@ module Discontent::PostsHelper
     html << content_tag(:ul, '', class: "discuss_comment", id: "note_form_#{post_content.id}_#{number_for_type_field(field)}") do
       post_content.post_notes(number_for_type_field(field)).each do |dpn|
         concat content_tag(:div,'', :id => "post_note_#{dpn.id}") {
-          (if link
-            link_to({:action => :destroy_note, :id => post_content.id,:note_id => dpn.id, :type_field => number_for_type_field(field)},:remote => true,:method => :put, :confirm => 'Удалить замечание?',:id => "destroy_post_note_#{dpn.id}") {content_tag(:span, '', class: "glyphicon glyphicon-remove text-danger pull-right")}
-          end) +
-          content_tag(:li, dpn.content, :id => "li_note_#{dpn.id}")
+          link ? link_to({:action => :destroy_note, :id => post_content.id,:note_id => dpn.id, :type_field => number_for_type_field(field)},:remote => true,:method => :put, :confirm => 'Удалить замечание?',:id => "destroy_post_note_#{dpn.id}") {content_tag(:span, '', class: "glyphicon glyphicon-remove text-danger pull-right")}  + content_tag(:li, dpn.content, :id => "li_note_#{dpn.id}") : content_tag(:li, dpn.content, :id => "li_note_#{dpn.id}")
         }
       end
     end
