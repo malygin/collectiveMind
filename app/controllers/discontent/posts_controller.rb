1f55ad4d82f17eacffa111b514d1e9804c56944c
irivertime <irivertime@mail.ru>
2014-09-18 00:43:43 +0400

    fix destroy group discontent

--- app/controllers/discontent/posts_controller.rb
+++ app/controllers/discontent/posts_controller.rb
@@ -156,7 +156,7 @@ class Discontent::PostsController < PostsController
     @union_post = Discontent::Post.find(params[:post_id])
     if @post.one_last_post? and boss?
       @union_post.update_attributes(status: 0, discontent_post_id: nil)
-      @post.destroy
+      @post.update_attributes(status: 3)
       @post.discontent_post_aspects.destroy_all
       return redirect_to action: "index"
     else
@@ -177,7 +177,7 @@ class Discontent::PostsController < PostsController
        end
      end
      if boss?
-       @post.destroy
+       @post.update_attributes(status: 3)
        @post.discontent_post_aspects.destroy_all
      end
      redirect_to action: "index"
