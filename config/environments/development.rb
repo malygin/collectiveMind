27dbd06cab7d3316fd9ec47f301eee07af5dcb38
Denis Karyakin <samhane.me@gmail.com>
2014-09-18 16:11:48 +0400

    Update config for development, production, test environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,73 +1,47 @@
 CollectiveMind::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
+  # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true
+  # Do not eager load code on boot.
+  config.eager_load = false
 
-  # Show full error reports and disable caching
-  config.consider_all_requests_local       = true
+  # Show full error reports and disable caching.
+  config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
+  # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  # Print deprecation notices to the Rails logger
+  # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  # Only use best-standards-support built into browsers
-  config.action_dispatch.best_standards_support = :builtin
+  # Raise an error on page load if there are pending migrations
+  config.active_record.migration_error = :page_load
 
-  # Raise exception on mass assignment protection for Active Record models
-  config.active_record.mass_assignment_sanitizer = :strict
-
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  config.active_record.auto_explain_threshold_in_seconds = 0.5
-
-  # Do not compress assets
-  config.assets.compress = false
-
-  # Send deprecation notices to registered listeners
-  config.active_support.deprecation = :notify
-  config.paperclip_defaults = {
-      :storage => :s3,
-      :s3_credentials => {
-          :bucket => 'stuff_bucket',
-          :access_key_id => 'AKIAIUXUHQCN45FUQ2UQ',
-          :secret_access_key => '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'
-      }
-  }
-
-  config.action_mailer.raise_delivery_errors = false
-
-  # Expands the lines which load the assets
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
   config.assets.debug = true
   config.quiet_assets = true
-  ::ActiveSupport::Deprecation.silenced = false
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.default :charset => 'utf-8'
-  #ActionMailer::Base.smtp_settings = {
-  #    :address              => 'smtp.gmail.com',
-  #    :port                 => 587,
-  #    :user_name            => 'massdecision@gmail.com',
-  #    :password             => '11nekotyan',
-  #    :authentication       => 'plain',
-  #    :enable_starttls_auto => true
-  #}
-  ActionMailer::Base.smtp_settings = {
-      :address              => 'smtp.yandex.com',
-      :port                 => 25,
-      :user_name            => 'mass-decision',
-      :password             => '11nekotyan',
-      :authentication       => 'plain',
-      :enable_starttls_auto => true
+
+  config.paperclip_defaults = {
+      storage: :s3,
+      s3_credentials: {
+          bucket: 'stuff_bucket',
+          access_key_id: 'AKIAIUXUHQCN45FUQ2UQ',
+          secret_access_key: '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'
+      }
   }
-  # Date::DATE_FORMATS[:default]="%-d %b %Y"
-  # Time::DATE_FORMATS[:default]="%Y/%m/%d %H:%M"
+  ActionMailer::Base.smtp_settings = {
+      address: 'smtp.yandex.com',
+      port: 25,
+      user_name: 'mass-decision',
+      password: '11nekotyan',
+      authentication: 'plain',
+      enable_starttls_auto: true
+  }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,125 +1,103 @@
 CollectiveMind::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
+  # Settings specified here will take precedence over those in config/application.rb.
 
-  # Code is not reloaded between requests
+  # Code is not reloaded between requests.
   config.cache_classes = true
 
-  # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
+  # Eager load code on boot. This eager loads most of Rails and
+  # your application in memory, allowing both thread web servers
+  # and those relying on copy on write to perform better.
+  # Rake tasks automatically ignore this option for performance.
+  config.eager_load = true
+
+  # Full error reports are disabled and caching is turned on.
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
-  #  # Disable Rails's static asset server (Apache or nginx will already do this)
-  #  config.serve_static_assets = false
-  #config.cache_store = :dalli_store
-  #config.action_dispatch.rack_cache = {
-  #  :metastore    => Dalli::Client.new,
-  #  :entitystore  => 'file:tmp/cache/rack/body',
-  #  :allow_reload => false
-  #}
-  config.serve_static_assets = true
+  # Enable Rack::Cache to put a simple HTTP cache in front of your application
+  # Add `rack-cache` to your Gemfile before enabling this.
+  # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
+  # config.action_dispatch.rack_cache = true
 
+  # Disable Rails's static asset server (Apache or nginx will already do this).
+  config.serve_static_assets = false
 
-  #config.cache_store = :dalli_store
-  #
-  #client = Dalli::Client.new(ENV["MEMCACHIER_SERVERS"], :value_max_bytes => 10485760,:expires_in => 86400) # 1 day
-  #
-  ## Configure rack-cache for using memcachier
-  #config.action_dispatch.rack_cache = {
-  #    :metastore    => client,
-  #    :entitystore  => client
-  #}
+  # Compress JavaScripts and CSS.
+  config.assets.js_compressor = :uglifier
+  # config.assets.css_compressor = :sass
 
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = false
 
-  config.static_cache_control = 'public, max-age=2592000'
-
-  # Compress JavaScripts and CSS
-  config.assets.compress = true
-
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
-
-  # Generate digests for assets URLs
+  # Generate digests for assets URLs.
   config.assets.digest = true
 
-  # Defaults to nil and saved in location specified by config.assets.prefix
-  # config.assets.manifest = YOUR_PATH
+  # Version of your assets, change this if you want to expire all your assets.
+  config.assets.version = '1.0'
 
-  # Specifies the header that your server uses for sending files
+  # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
-  # See everything in the log (default is :info)
-  # config.log_level = :debug
+  # Set to :debug to see everything in the log.
+  config.log_level = :info
 
-  # Prepend all log lines with the following tags
+  # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
-  # Use a different logger for distributed setups
+  # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
-  # Use a different cache store in production
+  # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server
+  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
 
-   config.assets.precompile += %w( google.js )
+  # Precompile additional assets.
+  # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+  # config.assets.precompile += %w( search.js )
 
-  # Disable delivery errors, bad email addresses will be ignored
+  # Ignore bad email addresses and do not raise email delivery errors.
+  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  # Enable threaded mode
-  # config.threadsafe!
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation can not be found)
+  # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
 
-  # Send deprecation notices to registered listeners
+  # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
+
+  # Disable automatic flushing of the log to improve performance.
+  # config.autoflush_log = false
+
+  # Use default logging formatter so that PID and timestamp are not suppressed.
+  config.log_formatter = ::Logger::Formatter.new
+
   config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => 'stuff_bucket',
-      :access_key_id => 'AKIAIUXUHQCN45FUQ2UQ',
-      :secret_access_key => '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'
-    }
+      storage: :s3,
+      s3_credentials: {
+          bucket: 'stuff_bucket',
+          access_key_id: 'AKIAIUXUHQCN45FUQ2UQ',
+          secret_access_key: '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'
+      }
   }
 
-  config.action_mailer.default_url_options = { :host => 'mass-decision.ru' }
+  config.action_mailer.default_url_options = {host: 'mass-decision.ru'}
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default :charset => 'utf-8'
+  config.action_mailer.default charset: 'utf-8'
   config.action_mailer.raise_delivery_errors = false
-
-  #ActionMailer::Base.smtp_settings = {
-  #    :address              => 'smtp.gmail.com',
-  #    :port                 => 587,
-  #    :user_name            => 'massdecision@gmail.com',
-  #    :password             => '11nekotyan',
-  #    :authentication       => 'plain',
-  #    :enable_starttls_auto => true
-  #}
-  # ActionMailer::Base.smtp_settings = {
-  #     :address              => 'smtp.yandex.com',
-  #     :port                 => 25,
-  #     :user_name            => 'mass-decision',
-  #     :password             => '11nekotyan',
-  #     :authentication       => 'plain',
-  #     :enable_starttls_auto => true
-  # }
   ActionMailer::Base.smtp_settings = {
-      :port =>           '587',
-      :address =>        'smtp.mandrillapp.com',
-      :user_name =>      ENV['MANDRILL_USERNAME'],
-      :password =>       ENV['MANDRILL_APIKEY'],
-      :domain =>         'mandrillapp.com',
-      :authentication => :plain
+      port: '587',
+      address: 'smtp.mandrillapp.com',
+      user_name: ENV['MANDRILL_USERNAME'],
+      password: ENV['MANDRILL_APIKEY'],
+      domain: 'mandrillapp.com',
+      authentication: :plain
   }
   ActionMailer::Base.delivery_method = :smtp
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
 end
