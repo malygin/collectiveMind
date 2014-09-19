7bf44d3a295f5548a7d3538a77075c02e2b56937
Andrey Malygin <anmalygin@gmail.com>
2014-09-19 23:53:37 +0400

    add turbo compile for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,6 @@ CollectiveMind::Application.configure do
 
   # Code is not reloaded between requests
   config.cache_classes = true
-
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
@@ -31,6 +30,8 @@ CollectiveMind::Application.configure do
 
 
   config.static_cache_control = 'public, max-age=2592000'
+
+  config.assets.debug = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
