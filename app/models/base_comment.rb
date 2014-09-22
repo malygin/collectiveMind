2559cf15e9502873355609d38e451ad82bbcbdc9
Denis Karyakin <samhane.me@gmail.com>
2014-09-22 14:11:06 +0400

    Reformat and replace old syntax in base_comment abd base_post

--- app/models/base_comment.rb
+++ app/models/base_comment.rb
@@ -1,24 +1,25 @@
-module BaseComment  extend ActiveSupport::Concern
+module BaseComment
+  extend ActiveSupport::Concern
   included do
     attr_accessible :content, :user, :censored, :post_id, :comment_id, :discontent_status, :concept_status, :discuss_status, :useful
     belongs_to :user
     belongs_to :post
 
-    has_many :comments, :foreign_key => 'comment_id'
-    belongs_to :comment, :foreign_key => 'comment_id'
+    has_many :comments, foreign_key: 'comment_id'
+    belongs_to :comment, foreign_key: 'comment_id'
 
     has_many :comment_votings
-    has_many :users, :through => :comment_votings
-        default_scope :order => 'created_at ASC'
+    has_many :users, through: :comment_votings
+    default_scope order: 'created_at ASC'
 
-    has_many :comment_votings_pro,:conditions => ['against = ?',false], :source => :comment_votings, :class_name => 'CommentVoting'
-    has_many :users_pro, :through => :comment_votings_pro, :source => :user
+    has_many :comment_votings_pro, conditions: ['against = ?', false], source: :comment_votings, class_name: 'CommentVoting'
+    has_many :users_pro, through: :comment_votings_pro, source: :user
 
-    has_many :comment_votings_against,:conditions => ['against = ?',true], :source => :comment_votings, :class_name => 'CommentVoting'
-    has_many :users_against, :through => :comment_votings_against, :source => :user
+    has_many :comment_votings_against, conditions: ['against = ?', true], source: :comment_votings, class_name: 'CommentVoting'
+    has_many :users_against, through: :comment_votings_against, source: :user
 
-    has_many :improve_disposts,:foreign_key => 'improve_comment', :conditions => {:improve_stage => [1,2]}, :source => :discontent_posts, :class_name => 'Discontent::Post'
-    has_many :improve_concepts,:foreign_key => 'improve_comment', :conditions => {:improve_stage => [1,2,3]}, :source => :concept_posts, :class_name => 'Concept::Post'
+    has_many :improve_disposts, foreign_key: 'improve_comment', conditions: {improve_stage: [1, 2]}, source: :discontent_posts, class_name: 'Discontent::Post'
+    has_many :improve_concepts, foreign_key: 'improve_comment', conditions: {improve_stage: [1, 2, 3]}, source: :concept_posts, class_name: 'Concept::Post'
 
     def get_class
       self.class.name.deconstantize
--- app/models/base_post.rb
+++ app/models/base_post.rb
@@ -1,39 +1,40 @@
-module BasePost  extend ActiveSupport::Concern
+module BasePost
+  extend ActiveSupport::Concern
   included do
-  	# status 0 - new, 1 -post expert, 2 - expeted, 3- archive
+    # status 0 - new, 1 -post expert, 2 - expeted, 3- archive
     attr_accessible :content, :status, :number_views, :user, :project, :censored, :useful
     belongs_to :user
-    belongs_to :project, :class_name => "Core::Project"
+    belongs_to :project, class_name: 'Core::Project'
     has_many :notes
     has_many :comments
 
-   	has_many :post_votings
-   	has_many :users, :through => :post_votings
+    has_many :post_votings
+    has_many :users, through: :post_votings
 
-    has_many :post_votings_pro,:conditions => ['against = ?',false], :source => :post_votings, :class_name => 'PostVoting'
-    has_many :users_pro, :through => :post_votings_pro, :source => :user
+    has_many :post_votings_pro, conditions: ['against = ?', false], source: :post_votings, class_name: 'PostVoting'
+    has_many :users_pro, through: :post_votings_pro, source: :user
 
-    has_many :post_votings_against,:conditions => ['against = ?',true], :source => :post_votings, :class_name => 'PostVoting'
-    has_many :users_against, :through => :post_votings_against, :source => :user
+    has_many :post_votings_against, conditions: ['against = ?', true], source: :post_votings, class_name: 'PostVoting'
+    has_many :users_against, through: :post_votings_against, source: :user
 
-    has_many :admins_pro, :through => :post_votings_pro, :source => :user,:conditions => {:users => {:type_user => [1,6]}}
-    has_many :admins_vote, :through => :post_votings, :source => :user,:conditions => {:users => {:type_user => [1,6]}}
+    has_many :admins_pro, through: :post_votings_pro, source: :user, conditions: {users: {type_user: [1, 6]}}
+    has_many :admins_vote, through: :post_votings, source: :user, conditions: {users: {type_user: [1, 6]}}
 
-    has_many :admins_against, :through => :post_votings_against, :source => :user ,:conditions => {:users => {:type_user => [1,6]}}
+    has_many :admins_against, through: :post_votings_against, source: :user, conditions: {users: {type_user: [1, 6]}}
 
-    scope :for_project, lambda { |project| where(:project_id => project) }
-    scope :for_expert, lambda {  where(:status => 1) }
-    scope :accepted, lambda {  where(:status => 2) }
-    scope :archive, lambda { where(:status => 3) }
-    scope :with_votes, -> {includes(:post_votings).where('"discontent_post_votings"."id" >0')}
-    scope :with_concept_votes, -> {includes(:post_votings).where('"concept_post_votings"."id" >0')}
+    scope :for_project, lambda { |project| where(project_id: project) }
+    scope :for_expert, lambda { where(status: 1) }
+    scope :accepted, lambda { where(status: 2) }
+    scope :archive, lambda { where(status: 3) }
+    scope :with_votes, -> { includes(:post_votings).where('"discontent_post_votings"."id" >0') }
+    scope :with_concept_votes, -> { includes(:post_votings).where('"concept_post_votings"."id" >0') }
 
-    scope :created_order,order("#{table_name}.created_at DESC")
-    scope :updated_order,order("#{table_name}.updated_at DESC")
+    scope :created_order, order("#{table_name}.created_at DESC")
+    scope :updated_order, order("#{table_name}.updated_at DESC")
     scope :popular_posts, order('number_views DESC')
 
     def show_content
-    	content
+      content
     end
 
     def self.order_by_param(order)
@@ -45,7 +46,7 @@ module BasePost  extend ActiveSupport::Concern
     end
 
     def main_comments
-      self.comments.where(:comment_id => nil)
+      self.comments.where(comment_id: nil)
     end
 
     def get_class
@@ -80,6 +81,5 @@ module BasePost  extend ActiveSupport::Concern
     def note_size?(type_field)
       self.post_notes(type_field).size > 0
     end
-
   end
 end
