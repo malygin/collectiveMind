9515b21bc37b953053627bd6a1b27a2232ce7eb7
Denis Karyakin <samhane.me@gmail.com>
2014-09-22 12:32:27 +0400

    Reformat code, and replace old ruby hash style to new in models/core/

--- app/models/core/project.rb
+++ app/models/core/project.rb
@@ -1,5 +1,3 @@
-# encoding: utf-8
-
 class Core::Project < ActiveRecord::Base
 ##### status 
 # 0 - prepare to procedure
@@ -31,43 +29,43 @@ class Core::Project < ActiveRecord::Base
 # 5 preparing procedure
 # 10 disabled
 
-  attr_accessible :desc,:postion, :secret, :type_project, :name, :short_desc, :knowledge, :status, :type_access, 
-  :url_logo, :stage1, :stage2, :stage3, :stage4, :stage5
+  attr_accessible :desc, :postion, :secret, :type_project, :name, :short_desc, :knowledge, :status, :type_access,
+                  :url_logo, :stage1, :stage2, :stage3, :stage4, :stage5
 
-  has_many :life_tape_posts, :class_name => "LifeTape::Post", :conditions => ['status = 0']
-  has_many :aspects, :class_name => "Discontent::Aspect"
+  has_many :life_tape_posts, class_name: 'LifeTape::Post', conditions: ['status = 0']
+  has_many :aspects, class_name: 'Discontent::Aspect'
 
-  has_many :discontents, :class_name => "Discontent::Post"
-  has_many :discontent_ongoing_post, :conditions =>"status = 0  ", :class_name => "Discontent::Post"
-  has_many :discontent_accepted_post, :conditions =>"status = 2  ", :class_name => "Discontent::Post"
-  has_many :discontent_for_admin_post, :conditions =>"status = 1  ", :class_name => "Discontent::Post"
+  has_many :discontents, class_name: 'Discontent::Post'
+  has_many :discontent_ongoing_post, conditions: 'status = 0  ', class_name: 'Discontent::Post'
+  has_many :discontent_accepted_post, conditions: 'status = 2  ', class_name: 'Discontent::Post'
+  has_many :discontent_for_admin_post, conditions: 'status = 1  ', class_name: 'Discontent::Post'
 
-  has_many :concepts, :class_name => "Concept::Post"
-  has_many :concept_ongoing_post, :conditions =>"status = 0  ", :class_name => "Concept::Post"
-  has_many :concept_accepted_post, :conditions =>"status = 2  ", :class_name => "Concept::Post"
-  has_many :concept_for_admin_post, :conditions =>"status = 1  ", :class_name => "Concept::Post"
+  has_many :concepts, class_name: 'Concept::Post'
+  has_many :concept_ongoing_post, conditions: 'status = 0  ', class_name: 'Concept::Post'
+  has_many :concept_accepted_post, conditions: 'status = 2  ', class_name: 'Concept::Post'
+  has_many :concept_for_admin_post, conditions: 'status = 1  ', class_name: 'Concept::Post'
 
-  has_many :plan_post, :conditions =>"status = 0", :class_name => "Plan::Post"
-  has_many :estimate_posts, :conditions =>"status = 0", :class_name => "Estimate::Post"
+  has_many :plan_post, conditions: 'status = 0', class_name: 'Plan::Post'
+  has_many :estimate_posts, conditions: 'status = 0', class_name: 'Estimate::Post'
 
   has_many :project_users
-  has_many :users, :through => :project_users
-  has_many :knowbase_posts, :class_name => 'Knowbase::Post'
+  has_many :users, through: :project_users
+  has_many :knowbase_posts, class_name: 'Knowbase::Post'
 
-  has_many :core_project_scores, :class_name => 'Core::ProjectScore'
+  has_many :core_project_scores, class_name: 'Core::ProjectScore'
 
-  has_many :core_project_users, :class_name => 'Core::ProjectUser'
-  has_many :users_in_project, :through => :core_project_users, :source => :user, :class_name => "User"
+  has_many :core_project_users, class_name: 'Core::ProjectUser'
+  has_many :users_in_project, through: :core_project_users, source: :user, class_name: 'User'
 
-  has_many :essays, :class_name => 'Essay::Post', :conditions => ['status = 0']
-  #has_many :project_score_users, :class_name => 'User', :through => :core_project_scores, :source => :user
-  scope :club_projects, ->(user){ where(type_access: 1) if user.cluber?  }
+  has_many :essays, class_name: 'Essay::Post', conditions: ['status = 0']
+  #has_many :project_score_users, class_name: 'User', through: :core_project_scores, source: :user
+  scope :club_projects, ->(user) { where(type_access: 1) if user.cluber? }
 
-  LIST_STAGES = {1 => {name: 'Сбор информации', :type_stage => :life_tape_posts, status: [0,1,2,20]},
-         2 => { name: 'Сбор несовершенств', :type_stage =>  :discontent_posts, status: [3,4,5,6]},
-         3 => { name: 'Сбор нововведений', :type_stage => :concept_posts, status: [7,8]},
-         4 => { name: 'Создание проектов', :type_stage =>  :plan_posts, status: [9]},
-         5 => { name: 'Выставление оценок', :type_stage =>  :estimate_posts, status: [10,11,12,13]}}.freeze
+  LIST_STAGES = {1 => {name: 'Сбор информации', type_stage: :life_tape_posts, status: [0, 1, 2, 20]},
+                 2 => {name: 'Сбор несовершенств', type_stage: :discontent_posts, status: [3, 4, 5, 6]},
+                 3 => {name: 'Сбор нововведений', type_stage: :concept_posts, status: [7, 8]},
+                 4 => {name: 'Создание проектов', type_stage: :plan_posts, status: [9]},
+                 5 => {name: 'Выставление оценок', type_stage: :estimate_posts, status: [10, 11, 12, 13]}}.freeze
 
 
   def current_aspects(current_stage)
@@ -81,6 +79,7 @@ class Core::Project < ActiveRecord::Base
   def stage1_count
     self.stage1 > self.proc_aspects.size ? self.proc_aspects.size : self.stage1
   end
+
   def get_free_votes_for(user, stage)
     case stage
       when 'lifetape'
@@ -94,17 +93,17 @@ class Core::Project < ActiveRecord::Base
     type_project = self.type_access
     type_user = user.type_user
 
-    if [1,7].include?(type_user) and [0,1,2,3,4,5].include?(type_project)
+    if [1, 7].include?(type_user) and [0, 1, 2, 3, 4, 5].include?(type_project)
       true
-    elsif [6].include?(type_user) and [0,1,3,4,5].include?(type_project)
+    elsif [6].include?(type_user) and [0, 1, 3, 4, 5].include?(type_project)
       true
-    elsif [2,3].include?(type_user) and [0,1,3].include?(type_project)
+    elsif [2, 3].include?(type_user) and [0, 1, 3].include?(type_project)
       true
-    elsif [4,5].include?(type_user) and [0,1,3].include?(type_project)
+    elsif [4, 5].include?(type_user) and [0, 1, 3].include?(type_project)
       true
-    elsif [8].include?(type_user) and [0,3].include?(type_project)
+    elsif [8].include?(type_user) and [0, 3].include?(type_project)
       true
-    elsif [0,3].include?(type_project)
+    elsif [0, 3].include?(type_project)
       true
     elsif [2].include?(type_project) and user.projects.include?(self)
       true
@@ -135,56 +134,59 @@ class Core::Project < ActiveRecord::Base
   end
 
   def proc_aspects
-    self.aspects.where(status:0)
+    self.aspects.where(status: 0)
   end
-  def get_united_posts_for_vote( user)
+
+  def get_united_posts_for_vote(user)
     voted = user.voted_discontent_posts.pluck(:id)
-    Discontent::Post.united_for_vote(self.id,voted)
+    Discontent::Post.united_for_vote(self.id, voted)
   end
+
   def get_concept_posts_for_vote(user)
     voted = user.concept_post_votings.pluck(:id)
-    Concept::Post.united_for_vote(self.id,voted)
+    Concept::Post.united_for_vote(self.id, voted)
   end
 
 
-  def current_status?( status)
-    sort_list  = LIST_STAGES.select {|k,v| v[:type_stage]  == status}
+  def current_status?(status)
+    sort_list = LIST_STAGES.select { |k, v| v[:type_stage] == status }
     sort_list.values[0][:status].include? self.status
   end
 
   def stage_style(status)
-     return 'disabled' if self.status < status_number(status)
-     return 'active' if current_status?(status)
+    return 'disabled' if self.status < status_number(status)
+    return 'active' if current_status?(status)
   end
 
   def stage_style_link(name_page, status)
-     return 'disabled' if self.status < status_number(status)
-     return 'current' if current_page?(name_page, status)
+    return 'disabled' if self.status < status_number(status)
+    return 'current' if current_page?(name_page, status)
   end
 
 
   def current_page?(page, status)
-    sort_list  = LIST_STAGES.select {|k,v| v[:type_stage]  == status}
+    sort_list = LIST_STAGES.select { |k, v| v[:type_stage] == status }
     sort_list.values[0][:name] == page
 
   end
 
   def redirect_to_current_stage
-    sort_list  = LIST_STAGES.select {|k,v| v[:status].include? self.status}
+    sort_list = LIST_STAGES.select { |k, v| v[:status].include? self.status }
     sort_list.values[0][:type_stage]
   end
+
   def can_edit_on_current_stage(p)
     if p.instance_of? LifeTape::Post
-        return true
-     elsif p.instance_of? Discontent::Post
-       return self.status == 3
-     elsif p.instance_of? Concept::Post
-       return self.status == 7
-     elsif p.instance_of? Plan::Post
-       return self.status == 9
-     elsif p.instance_of? Estimate::Post
-       return self.status == 10
-     end
+      return true
+    elsif p.instance_of? Discontent::Post
+      return self.status == 3
+    elsif p.instance_of? Concept::Post
+      return self.status == 7
+    elsif p.instance_of? Plan::Post
+      return self.status == 9
+    elsif p.instance_of? Estimate::Post
+      return self.status == 10
+    end
     return false
   end
 
@@ -200,7 +202,7 @@ class Core::Project < ActiveRecord::Base
         9
       when :estimate_posts
         10
-     end
+    end
   end
 
   def model_min_stage(model)
@@ -224,7 +226,7 @@ class Core::Project < ActiveRecord::Base
     self.type_access == 3
   end
 
-  def  status_title(status = self.status)
+  def status_title(status = self.status)
     case status
       when 0
         'подготовка к процедуре'
@@ -267,7 +269,7 @@ class Core::Project < ActiveRecord::Base
     discontent_comments = Discontent::Comment.joins("INNER JOIN discontent_posts ON discontent_comments.post_id = discontent_posts.id").
         where("discontent_posts.project_id = ? and discontent_comments.discontent_status = 't'", self.id)
     comments_all = life_tape_comments | discontent_comments
-    comments_all.sort_by{|c| c.improve_disposts.size}
+    comments_all.sort_by { |c| c.improve_disposts.size }
   end
 
   def ideas_comments_for_improve
@@ -278,8 +280,6 @@ class Core::Project < ActiveRecord::Base
     concept_comments = Concept::Comment.joins("INNER JOIN concept_posts ON concept_comments.post_id = concept_posts.id").
         where("concept_posts.project_id = ? and concept_comments.concept_status = 't'", self.id)
     comments_all = life_tape_comments | discontent_comments | concept_comments
-    comments_all.sort_by{|c| c.improve_concepts.size}
+    comments_all.sort_by { |c| c.improve_concepts.size }
   end
-
-
 end
--- app/models/core/project_score.rb
+++ app/models/core/project_score.rb
@@ -1,8 +1,8 @@
 class Core::ProjectScore < ActiveRecord::Base
   attr_accessible :project_id, :score, :score_a, :score_g, :score_o, :user_id
-  belongs_to :core_project,  :class_name => "Core::Project",  :foreign_key => "project_id"
+  belongs_to :core_project, class_name: 'Core::Project', foreign_key: 'project_id'
   belongs_to :user
 
-  scope :user_scores, ->(pr,sn) { where(:project_id => pr).where("#{sn}>0").order("#{sn} DESC") }
-  scope :by_project, ->(pr){where(:project_id => pr)}
+  scope :user_scores, ->(pr, sn) { where(project_id: pr).where("#{sn}>0").order("#{sn} DESC") }
+  scope :by_project, ->(pr) { where(project_id: pr) }
 end
