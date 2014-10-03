c80c56b3ea1be8c1d115bf5a27dffbc0d8318eb2
Denis Karyakin <samhane.me@gmail.com>
2014-10-03 16:55:53 +0400

    Refactor moderator_chat controller with using current_user from devise

--- app/controllers/moderator_chat_controller.rb
+++ app/controllers/moderator_chat_controller.rb
@@ -1,21 +1,17 @@
 class ModeratorChatController < WebsocketRails::BaseController
-  before_filter :set_current_user
-
   def initialize_session
   end
 
   def user_connected
-    p 'user connected'
     send_message :user_info, {user: 'current_user.firstname'}
   end
 
   def incoming_message
-    # moderator_message = @current_user.moderator_messages.create message: message[:text]
-    # broadcast_message :new_message, {user: "#{@current_user.name} #{@current_user.surname}",
-    #                                  avatar: @current_user.avatar(:thumb), text: moderator_message.message,
-    #                                  id: moderator_message.id,
-    #                                  time: Russian::strftime(moderator_message.created_at, '%k:%M:%S')}
-    broadcast_message :new_message, {text: message[:text], id: (1 + Random.rand(9999))}
+    moderator_message = current_user.moderator_messages.create message: message[:text]
+    broadcast_message :new_message, {user: "#{current_user.name} #{current_user.surname}",
+                                     avatar: current_user.avatar(:thumb), text: moderator_message.message,
+                                     id: moderator_message.id,
+                                     time: Russian::strftime(moderator_message.created_at, '%k:%M:%S')}
   end
 
   def user_disconnected
@@ -23,16 +19,10 @@ class ModeratorChatController < WebsocketRails::BaseController
   end
 
   def authorize_channels
-    session.inspect
-    if true
+    if current_user.admin?
       accept_channel
     else
       deny_channel
     end
-  end
-
-  private
-  def set_current_user
-    @current_user = warden.authenticate
   end
 end
