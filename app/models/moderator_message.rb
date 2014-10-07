a6bb4038be3938639fb7e95f05a5b5acc3261701
Denis Karyakin <samhane.me@gmail.com>
2014-10-07 15:18:10 +0400

    Add scope recent to model moderator message (with limit in 10 last messages), create method time for unified show time of created message

--- app/models/moderator_message.rb
+++ app/models/moderator_message.rb
@@ -2,4 +2,10 @@ class ModeratorMessage < ActiveRecord::Base
   #@todo remove
   attr_accessible :message
   belongs_to :user
+
+  scope :recent, -> { limit(10) }
+
+  def time
+    Russian::strftime(created_at, '%k:%M:%S')
+  end
 end
