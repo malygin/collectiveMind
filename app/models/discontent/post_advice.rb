3e7d09829b6036e82464fbefc896afdd06dbf453
Denis Karyakin <samhane.me@gmail.com>
2014-10-09 22:31:52 +0400

    Show unapproved advices in essays

--- app/models/discontent/post_advice.rb
+++ app/models/discontent/post_advice.rb
@@ -3,4 +3,6 @@ class Discontent::PostAdvice < ActiveRecord::Base
   belongs_to :discontent_post, class_name: 'Discontent::Post'
   #@todo remove
   attr_accessible :content
+
+  scope :unapproved, -> { where approved: false }
 end
