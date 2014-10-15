cf39a07b00620b1475b2d8f504bfbfb2f5d9e7ef
Denis Karyakin <samhane.me@gmail.com>
2014-10-16 00:45:34 +0400

    Add advices on concept show page

--- app/models/concept/post.rb
+++ app/models/concept/post.rb
@@ -14,6 +14,7 @@ class Concept::Post < ActiveRecord::Base
   has_many :concept_disposts, through: :concept_post_discontents, source: :discontent_post, class_name: 'Discontent::Post'
   has_many :concept_post_resources, class_name: 'Concept::PostResource'
   has_many :concept_post_discontent_grouped, -> { where concept_post_discontents: {status: [1]} }, class_name: 'Concept::PostDiscontent'
+  has_many :advices, class_name: 'Advice', as: :adviseable
 
   scope :stat_fields_negative, ->(p) { where(id: p).where("status_name = 'f' or status_content = 'f' or status_negative = 'f'
             or status_positive = 'f' or status_control = 'f' or status_obstacles = 'f' or status_reality = 'f' or status_problems = 'f' ") }
