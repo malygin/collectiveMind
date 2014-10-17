9195d060935edaedba09c4b9c353a8114ed174df
Denis Karyakin <samhane.me@gmail.com>
2014-10-17 15:26:26 +0400

    Fix specs, but not all realized

--- spec/requests/advices_spec.rb
+++ spec/requests/advices_spec.rb
@@ -9,7 +9,8 @@ describe 'Advices' do
 
   before do
     prepare_concepts(project, user)
-    @advice = create :advice, user: user, adviseable: @discontent1
+    @advice_unapproved = create :advice_unapproved, user: user, adviseable: @discontent1
+    @advice = create :advice_approved, user: user, adviseable: @discontent1
   end
 
   context 'prime admin can setup' do
@@ -45,10 +46,10 @@ describe 'Advices' do
   context 'ordinary user sign in ' do
     before do
       sign_in user
-      visit discontent_posts_path(project)
+      visit discontent_post_path(project, @discontent1)
     end
 
-    context 'unapproved' do
+    context 'not view unapproved' do
       it 'not link in left side' do
         within :css, 'ul#side-nav' do
           expect(page).not_to have_content I18n.t('left_side.discontent_post_advices')
@@ -64,7 +65,6 @@ describe 'Advices' do
 
     it 'create', js: true do
       text_advice = 'Очень хороший совет'
-      visit discontent_post_path(project, @discontent1)
       expect {
         fill_in 'advice_content', with: text_advice
         click_button 'send_advice'
@@ -73,29 +73,59 @@ describe 'Advices' do
       }.to change(Advice.unapproved, :count).by(1)
     end
 
+    it 'not view unapproved in list' do
+      advice_unapproved = create :advice_unapproved, user: moderator, adviseable: @discontent1
+      visit discontent_post_path(project, @discontent1)
+      expect(page).not_to have_content advice_unapproved.content
+      expect(page).to have_content @advice.content
+    end
+
+    it 'edit' do
+
+    end
+
+    context 'correct link to advisable' do
+      it 'to discontent' do
+        #@todo
+        # within :css, "#post_advice_#{@advice_unapproved.id}" do
+        #   expect(page).to have_content @advice_unapproved.adviseable.content
+        # end
+        # click_link "open_post_#{@advice_unapproved.adviseable.id}"
+        # expect(current_path) == discontent_post_path(project, @advice_unapproved.adviseable)
+      end
+      it 'to concept'
+    end
+
     context 'remove' do
       it 'if author - ok', js: true do
-        advice = create :advice, user: user, adviseable: @discontent1
-        visit discontent_post_path(project, @discontent1)
         expect {
-          click_link "remove_advice_#{advice.id}"
+          click_link "remove_advice_#{@advice_unapproved.id}"
           page.driver.browser.accept_js_confirms
-          expect(page).not_to have_content advice.content
+          expect(page).not_to have_content @advice_unapproved.content
         }.to change(Advice, :count).by(-1)
       end
 
       it 'others - no' do
-        advice = create :advice, user: moderator, adviseable: @discontent1
+        advice = create :advice_approved, user: moderator, adviseable: @discontent1
         visit discontent_post_path(project, @discontent1)
+        expect(page).to have_content advice.content
         expect(page).not_to have_link "remove_advice_#{advice.id}"
       end
     end
+
+    context 'show in news'
+    context 'show in personal notifications'
+    context 'receive balls'
+
+    context 'discuss with moderator'
+
+    context 'set useful'
   end
 
   context 'moderator sign in' do
     before do
       sign_in moderator
-      visit discontent_advices_path(project)
+      visit advices_path(project)
     end
 
     it 'link to list unapproved advices (with count of it)' do
@@ -109,57 +139,40 @@ describe 'Advices' do
 
     it 'list unapproved advices' do
       click_link 'open_unapproved_advices'
-      expect(current_path) == discontent_advices_path(project, @advice)
-      expect(page).to have_content @advice.content
-      expect(page).to have_content @advice.user
-    end
-
-    it 'link to discontent' do
-      within :css, "#post_advice_#{@advice.id}" do
-        expect(page).to have_content @advice.adviseable.content
-      end
-      click_link "open_post_#{@advice.adviseable.id}"
-      expect(current_path) == discontent_post_path(project, @advice.adviseable)
+      expect(current_path) == advices_path(project)
+      expect(page).to have_content @advice_unapproved.content
+      expect(page).to have_content @advice_unapproved.user
     end
 
     it 'approve advice', js: true do
       expect {
-        click_link "approve_advice_#{@advice.id}"
+        click_link "approve_advice_#{@advice_unapproved.id}"
         expect(page).to have_content I18n.t('discontent.advice_success_approved')
-        expect(page).not_to have_content @advice.content
+        expect(page).not_to have_content @advice_unapproved.content
       }.to change(Advice.unapproved, :count).by(-1)
     end
 
     it 'delete any advice', js: true do
       expect {
-        click_link "remove_advice_#{@advice.id}"
+        click_link "remove_advice_#{@advice_unapproved.id}"
         page.driver.browser.accept_js_confirms
-        expect(page).not_to have_content @advice.content
+        expect(page).not_to have_content @advice_unapproved.content
       }.to change(Advice.unapproved, :count).by(-1)
     end
 
     context 'discuss with author advice', js: true do
       before do
-        @advice = create :advice, user: user, adviseable: @discontent1
-        @comment = create :advice_comment, user: user, advice: @advice
-        visit discontent_advice_path(project, @advice)
+        @comment = create :advice_comment, user: user, advice: @advice_unapproved
+        visit advices_path(project)
       end
 
       it 'create comment' do
         text_comment = 'Хороший совет, но нужно обсудить'
         expect {
-          fill_in 'comment_text_area', with: text_comment
-          click_button 'send_comment'
+          fill_in "comment_text_for_#{@advice_unapproved.id}", with: text_comment
+          click_button "send_comment_for_#{@advice_unapproved.id}"
           expect(page).to have_content text_comment
         }.to change(AdviceComment, :count).by(1)
-      end
-
-      it 'remove' do
-        expect {
-          click_link "destroy_comment_#{@comment.id}"
-          page.driver.browser.accept_js_confirms
-          expect(page).not_to have_content @comment.content
-        }.to change(AdviceComment, :count).by(-1)
       end
     end
   end
