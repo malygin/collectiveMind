d1c755b1f0978a8c52e9ce08d2a5af5d6fc4cfa1
Denis Karyakin <samhane.me@gmail.com>
2014-10-17 15:25:16 +0400

    Refactor factories

--- spec/factories/advice.rb
+++ spec/factories/advice.rb
@@ -1,5 +1,13 @@
 FactoryGirl.define do
-  factory :advice, class: 'Advice'  do
+  factory :advice_unapproved, class: 'Advice' do
     sequence(:content) { |n| "Advice number #{n}" }
+
+    factory :advice_approved do
+      approved true
+    end
+  end
+
+  factory :advice_comment, class: 'AdviceComment' do
+    sequence(:content) { |n| "Comment to advice number #{n}" }
   end
 end
