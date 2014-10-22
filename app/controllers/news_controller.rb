846c24f16ecf3da9261f4c500fa4ea39deda9503
Denis Karyakin <samhane.me@gmail.com>
2014-10-22 22:16:19 +0400

    Try to fix authorize_channels for heroku

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -4,10 +4,11 @@ class NewsController < WebsocketRails::BaseController
   end
 
   def authorize_channels
-    if current_user.nil?
-      deny_channel
-    else
-      accept_channel current_user
-    end
+    accept_channel
+    # if current_user.nil?
+    #   deny_channel
+    # else
+    #   accept_channel current_user
+    # end
   end
 end
