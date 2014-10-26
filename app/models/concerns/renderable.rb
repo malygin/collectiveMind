2559c771478f0d69b92b86a56de22419e0b851e8
Denis Karyakin <samhane.me@gmail.com>
2014-10-26 21:53:48 +0300

    Fix renderable

--- app/models/concerns/renderable.rb
+++ app/models/concerns/renderable.rb
@@ -1,12 +1,17 @@
 module Renderable
   extend ActiveSupport::Concern
-  delegate :url_helpers, to: 'Rails.application.routes'
 
   private
   def render_anywhere(partial, locals = {})
-    ApplicationController.new.render_to_string(
-        partial: partial,
-        locals: locals
-    )
+    action_view = ActionView::Base.new(Rails.configuration.paths['app/views'])
+    action_view.class_eval do
+      include Rails.application.routes.url_helpers
+      include JournalHelper
+
+      def protect_against_forgery?
+        false
+      end
+    end
+    action_view.render partial: partial, locals: locals
   end
 end
