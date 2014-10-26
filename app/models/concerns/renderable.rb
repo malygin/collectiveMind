4bd48bdec86215ac77057d1caecb65af48275c54
Denis Karyakin <samhane.me@gmail.com>
2014-10-26 19:13:12 +0300

    Clean renderable from comments and unused parameters

--- app/models/concerns/renderable.rb
+++ app/models/concerns/renderable.rb
@@ -1,16 +1,12 @@
 module Renderable
   extend ActiveSupport::Concern
-  #include Rails.application.routes.url_helpers
   delegate :url_helpers, to: 'Rails.application.routes'
 
   private
-  def render_anywhere(partial, locals = {}, assigns = {})
+  def render_anywhere(partial, locals = {})
     ApplicationController.new.render_to_string(
         partial: partial,
         locals: locals
     )
-    # view = ActionView::Base.new ActionController::Base.view_paths, assigns
-    # view.extend JournalHelper
-    # view.render partial: partial, locals: locals
   end
 end
