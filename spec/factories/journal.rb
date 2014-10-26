4f35b8fc3983522479d203d4b2583bddf7c23eb2
Denis Karyakin <samhane.me@gmail.com>
2014-10-26 17:27:07 +0300

    Create factory personal_journal and write specs to journal_clean

--- spec/factories/journal.rb
+++ spec/factories/journal.rb
@@ -1,9 +1,13 @@
 FactoryGirl.define do
-  factory :journal, :class => 'Journal' do
+  factory :journal, class: 'Journal' do
     type_event 'life_tape_comment_save'
     sequence(:body) { |n| "body #{n}" }
     sequence(:body2) { |n| "body2 #{n}" }
     viewed false
     personal false
+
+    factory :personal_journal do
+      personal true
+    end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,8 +1,49 @@
-# encoding: utf-8
 require 'spec_helper'
-describe 'Users ' do
-  context  'ordinary user sign in ' do
 
+describe 'Users ' do
+  subject { page }
+  let (:user) { create :user }
+  let (:project) { create :core_project, status: 1, advices_concept: true, advices_discontent: true }
+
+  context 'ordinary user sign in ' do
+    before do
+      sign_in user
+    end
+
+    context 'my journal', js: true do
+      before do
+        @personal_journal = create :personal_journal, project: project, user: user, user_informed: user
+        visit "/project/#{project.id}"
+      end
+
+      it 'have count' do
+        within :css, 'a#messages span.count' do
+          expect(page).to have_content '1'
+        end
+      end
+
+      it 'have content on click' do
+        click_link 'messages'
+        within :css, 'ul#messages-menu' do
+          expect(page).to have_content @personal_journal.body
+        end
+      end
+
+      context 'clear' do
+        before do
+          click_link 'messages'
+          click_link 'clear_my_journals'
+        end
+
+        it { expect change(Journal.events_for_my_feed(project, user), :count).by(-1) }
+
+        it 'on page' do
+          expect(page).not_to have_content @personal_journal
+          expect(page).not_to have_link 'clear_my_journals'
+          expect(page).to have_content I18n.t('menu.journal_empty')
+        end
+      end
+    end
   end
 
   context 'moderator sign in ' do
