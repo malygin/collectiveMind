429ad7d4b2afd9f04c659c2a46605f2f8d614fe8
irivertime <irivertime@mail.ru>
2014-11-02 15:26:32 +0300

    fix merge

--- db/schema.rb
+++ db/schema.rb
@@ -365,10 +365,10 @@ ActiveRecord::Schema.define(version: 20141101155052) do
     t.string   "secret"
     t.string   "secret2"
     t.string   "secret3"
-    t.boolean  "advices_discontent"
-    t.boolean  "advices_concept"
     t.string   "color"
     t.string   "code"
+    t.boolean  "advices_discontent"
+    t.boolean  "advices_concept"
   end
 
   add_index "core_projects", ["status"], name: "index_core_projects_on_status", using: :btree
