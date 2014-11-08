dba26270a6db15feb65385d8f78b3d621f57df8c
Denis Karyakin <samhane.me@gmail.com>
2014-11-08 23:32:51 +0300

    Create relations between group and user, create scope for filtering by project

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,8 @@ class Group < ActiveRecord::Base
   #@todo remove
   attr_accessible :name, :description, :project_id
   belongs_to :project
+  has_many :group_users
+  has_many :users, through: :group_users
+
+  scope :by_project, -> (project) { where project_id: project.id }
 end
