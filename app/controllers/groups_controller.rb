1831c1f2c95fc646a2e2fcff35bf7d049f268c5a
Denis Karyakin <samhane.me@gmail.com>
2014-11-09 01:39:27 +0300

    Realize become_member and leave actions

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,8 @@
 class GroupsController < ApplicationController
   before_action :journal_data
-  before_action :set_group, only: [:show, :edit, :update, :destroy]
+  before_action :set_group, except: [:index, :new, :create]
   before_filter :check_admin, only: [:new, :edit, :create, :update, :destroy]
+  before_filter :only_members, only: :show
 
   # GET /groups
   def index
@@ -35,7 +36,7 @@ class GroupsController < ApplicationController
   # PATCH/PUT /groups/1
   def update
     if @group.update(group_params)
-      redirect_to @group, notice: 'Group was successfully updated.'
+      redirect_to groups_url(@project, @group)
     else
       render :edit
     end
@@ -44,10 +45,25 @@ class GroupsController < ApplicationController
   # DELETE /groups/1
   def destroy
     @group.destroy
-    redirect_to groups_url, notice: 'Group was successfully destroyed.'
+    redirect_to groups_url(@project, @group)
+  end
+
+  def become_member
+    @group.group_users.new user_id: current_user.id
+    @group.save
+    redirect_to group_path(@project, @group)
+  end
+
+  def leave
+    current_user.group_users.where(group_id: @group.id).destroy
+    redirect_to groups_path(@project)
   end
 
   private
+  def only_members
+    redirect_back_or project_path(@project) unless @group.users.include? current_user
+  end
+
   def check_admin
     redirect_back_or project_path(@project) unless current_user.admin?
   end
@@ -55,7 +71,7 @@ class GroupsController < ApplicationController
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_group
-    @group = Group.find(params[:id])
+    @group = Group.find params[:id] || params[:group_id]
   end
 
   # Only allow a trusted parameter "white list" through.
