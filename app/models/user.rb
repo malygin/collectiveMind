c8b5cefa98642941f0c2affb7f3e95e8001f0f7e
Denis Karyakin <samhane.me@gmail.com>
2014-11-10 00:59:55 +0300

    Create model for saving group chat messages, link with user

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,6 +54,7 @@ class User < ActiveRecord::Base
   has_many :user_checks, class_name: 'UserCheck'
   has_many :group_users
   has_many :groups, through: :group_users
+  has_many :group_chat_messages
 
   scope :check_field, ->(p, c) { where(project: p.id, status: 't', check_field: c) }
   scope :without_added, ->(users) { where("users.id NOT IN (#{users.join(", ")})") unless users.empty? }
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141108173213) do
+ActiveRecord::Schema.define(version: 20141109213228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -904,6 +904,17 @@ ActiveRecord::Schema.define(version: 20141108173213) do
   add_index "frustrations", ["created_at"], name: "index_frustrations_on_created_at", using: :btree
   add_index "frustrations", ["status"], name: "index_frustrations_on_status", using: :btree
   add_index "frustrations", ["user_id"], name: "index_frustrations_on_user_id", using: :btree
+
+  create_table "group_chat_messages", force: true do |t|
+    t.integer  "group_id"
+    t.integer  "user_id"
+    t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "group_chat_messages", ["group_id"], name: "index_group_chat_messages_on_group_id", using: :btree
+  add_index "group_chat_messages", ["user_id"], name: "index_group_chat_messages_on_user_id", using: :btree
 
   create_table "group_users", force: true do |t|
     t.integer  "group_id"
