928b9edaaa8e8a08a888f42a6eb7d92961543805
Denis Karyakin <samhane.me@gmail.com>
2014-11-24 00:07:13 +0300

    Rewrite relation group with group_user

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,8 @@ class Group < ActiveRecord::Base
   #@todo remove
   attr_accessible :name, :description, :project_id
   belongs_to :project, class_name: 'Core::Project'
-  has_many :group_users, -> { where invite_accepted: true }, dependent: :destroy
+  has_many :all_group_users, class_name: 'GroupUser', dependent: :destroy
+  has_many :group_users, -> { where invite_accepted: true }
   has_many :users, through: :group_users
   has_many :chat_messages, class_name: 'GroupChatMessage'
 
