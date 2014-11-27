fde6822f39f671fbd9ba4499aef8eb3dbf634650
Denis Karyakin <samhane.me@gmail.com>
2014-11-27 22:37:43 +0300

    Rewrite relation in group task with users and group task users

--- app/models/group_task.rb
+++ app/models/group_task.rb
@@ -1,6 +1,7 @@
 class GroupTask < ActiveRecord::Base
   belongs_to :group
-  has_many :users, class_name: 'GroupTaskUser'
+  has_many :group_task_users, class_name: 'GroupTaskUser'
+  has_many :users, through: :group_task_users
   attr_accessible :name, :description, :group_id
 
   validates :name, :description, presence: true
