a8b29e86b7291e93aabd8e12f01b644c7753aa9a
Denis Karyakin <samhane.me@gmail.com>
2014-11-27 18:28:14 +0300

    Create relation between group_task and group_task_users, draft out it on each task

--- app/models/group_task.rb
+++ app/models/group_task.rb
@@ -1,5 +1,6 @@
 class GroupTask < ActiveRecord::Base
   belongs_to :group
+  has_many :users, class_name: 'GroupTaskUser'
   attr_accessible :name, :description, :group_id
 
   validates :name, :description, presence: true
