35ce83bcd17a9f415cc37781a5800daf0e8e47a8
Denis Karyakin <samhane.me@gmail.com>
2014-12-07 17:20:48 +0300

    Show project from group users

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,6 +5,7 @@ class Group < ActiveRecord::Base
   has_many :all_group_users, class_name: 'GroupUser', dependent: :destroy
   has_many :group_users, -> { where invite_accepted: true }
   has_many :users, through: :group_users
+  has_many :user_plan_posts, through: :users, source: :plan_posts
   has_many :chat_messages, class_name: 'GroupChatMessage'
   has_many :tasks, class_name: 'GroupTask'
 
@@ -18,6 +19,10 @@ class Group < ActiveRecord::Base
   }
   validates :name, :project_id, presence: true
   validates :status, inclusion: {in: STATUSES.keys}
+
+  def user_projects
+    user_plan_posts.by_project(project_id)
+  end
 
   def owner
     group_users.find_by(owner: true).try(:user)
--- app/models/user.rb
+++ app/models/user.rb
@@ -56,6 +56,7 @@ class User < ActiveRecord::Base
   has_many :group_users
   has_many :groups, through: :group_users
   has_many :group_chat_messages
+  has_many :plan_posts, class_name: 'Plan::Post'
 
   scope :check_field, ->(p, c) { where(project: p.id, status: 't', check_field: c) }
   scope :without_added, ->(users) { where("users.id NOT IN (#{users.join(", ")})") unless users.empty? }
