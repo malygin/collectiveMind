9787aa0fd3cb2dce9664935004e1cc9d77387fc7
Denis Karyakin <samhane.me@gmail.com>
2014-12-10 00:43:29 +0300

    Working start_edit for groups actions. Additional methods: redis, redis_key and editable

--- app/controllers/group_actions_controller.rb
+++ app/controllers/group_actions_controller.rb
@@ -1,52 +1,53 @@
 class GroupActionsController < WebsocketRails::BaseController
-  def initialize_session
-    p '========================initialize'
-    p controller_store[:editing]
-    controller_store[:editing] = {} if controller_store[:editing].nil?
-  end
-
-  def client_connected
-    p '========================CONNECTED!'
-  end
-
   def start_edit
-    p controller_store[:editing]
-    flag = false
-    if controller_store[:editing][message[:model_name]].present?
-      controller_store[:editing][message[:model_name]].each do |current_model|
-        if current_model[:model_id] == message[:model_id] && current_model[:user_id] != current_user.id
-          WebsocketRails.users[current_user.id].send_message(:already_editing, {model_name: message[:model_name], model_id: message[:model_id]}, channel: 'group.actions')
-          break
-        else
-          flag = true
+    no_edits = true
+    if editable[message[:model_name]].present?
+      JSON.parse(editable[message[:model_name]]).each do |current_model|
+        if current_model['model_id'] == message[:model_id]
+          unless current_model['user_id'] == current_user.id
+            WebsocketRails.users[current_user.id].send_message(:already_editing, {model_name: message[:model_name], model_id: message[:model_id]}, channel: 'group.actions')
+            no_edits = false
+            break
+          end
         end
       end
-    else
-      flag = true
     end
 
-    if flag
-      controller_store[:editing][message[:model_name]] = Array.new
-      controller_store[:editing][message[:model_name]] << {model_id: message[:model_id], user_id: current_user.id}
+    if no_edits
+      if editable[message[:model_name]].present?
+        editing_array = JSON.parse(editable[message[:model_name]])
+      else
+        editing_array = Array.new
+      end
+      editing_array << {model_id: message[:model_id], user_id: current_user.id}
+      redis.hset redis_key, message[:model_name], editing_array.to_json
     end
-    WebsocketRails::Synchronization.redis.hset 'websocket_rails.editable', message[:model_name], controller_store[:editing][message[:model_name]].to_json
-
-    p '========================start edit finish'
-    p controller_store.collect_all :editing
-    p controller_store[:editing]
   end
 
   def client_disconnected
-    p controller_store[:editing]
-    controller_store[:editing].each do |current_edit|
-      current_edit[1].each do |current_models|
-        if current_models[:user_id] == current_user.id
-          current_edit[1].delete current_models
-        end
-      end
+    # editable.each do |current_edit|
+    #   tmp = []
+    #   save_again = false
+    #   current_edit.each do |current_model|
+    #     if current_model['user_id'] != current_user.id
+    #       tmp << current_model
+    #       save_again = true
+    #     end
+    #   end
+    #   redis.hset(redis_key, current_edit[0], tmp[1].to_json) if save_again && current_edit
+    # end
+  end
 
-    end
-    p '========================user disconnected'
-    p controller_store[:editing]
+  private
+  def redis
+    @redis ||= Redis.new(WebsocketRails.config.redis_options)
+  end
+
+  def redis_key
+    'websocket_rails.editable'
+  end
+
+  def editable
+    redis.hgetall redis_key
   end
 end
