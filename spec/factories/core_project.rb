8901842bc8445d253c9e57b9c6932027263a7706
Denis Karyakin <samhane.me@gmail.com>
2014-12-15 14:12:43 +0300

    Fix factories for project

--- spec/factories/core_project.rb
+++ spec/factories/core_project.rb
@@ -3,6 +3,14 @@ FactoryGirl.define do
     sequence(:name) { |n| "Project #{n}" }
     type_access 0
     status 1
+
+    factory :closed_project do
+      type_access Core::Project::TYPE_ACCESS_CODE[:closed]
+    end
+
+    factory :club_project do
+      type_access Core::Project::TYPE_ACCESS_CODE[:club]
+    end
   end
 
   factory :core_project_user, class: 'Core::ProjectUser' do
