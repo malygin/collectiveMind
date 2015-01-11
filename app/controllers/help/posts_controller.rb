0deb87dcb3e0e74697bf4ab07cd36d9b36ccd76d
irivertime <irivertime@mail.ru>
2015-01-11 15:50:03 +0300

    add slides and helps

--- app/controllers/help/posts_controller.rb
+++ app/controllers/help/posts_controller.rb
@@ -17,7 +17,7 @@ class Help::PostsController < PostsController
   def show
     @stages = Core::Project::LIST_STAGES
     @posts = {}
-    @help_partial = case params[:id]
+    @help_partial_1 = case params[:id]
                       when '0'
                          'new_help_0'
                       when '1'
@@ -35,7 +35,24 @@ class Help::PostsController < PostsController
                       else
                         'new_help_0'
                     end
-
+    @help_partial_2 = case params[:id]
+                      when '0'
+                        'new_help_0'
+                      when '1'
+                        'new_help_1_s'
+                      when '2'
+                        'new_help_2_s'
+                      when '3'
+                        'new_help_3_s'
+                      when '4'
+                        'new_help_4_s'
+                      when '5'
+                        'new_help_5_s'
+                      when '6'
+                        'new_help_6'
+                      else
+                        'new_help_0'
+                    end
     # Help::Post.where(stage: id, mini: false).each {|f| @posts[f.style] = f }
   end
 end
