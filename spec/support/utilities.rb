301d948c4d93b8dd3101c2705339f6ef9d3e254c
Denis Karyakin <samhane.me@gmail.com>
2015-01-15 23:28:24 +0300

    Remove prepare_plans from utilities, remove already setted fields

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -418,32 +418,11 @@ def prepare_concepts(project, user)
 
   @concept1 = create :concept, user: user, project: project
   @concept2 = create :concept, user: user, project: project
-  @concept_aspect1 = create :concept_aspect, discontent_aspect_id: @discontent1.id, concept_post_id: @concept1.id, positive: 'positive 1', negative: 'negative 1', title: 'title 1', control: 'control 1', content: 'content 1', reality: 'reality 1', problems: 'problems 1', name: 'name 1'
-  @concept_aspect2 = create :concept_aspect, discontent_aspect_id: @discontent1.id, concept_post_id: @concept2.id, positive: 'positive 2', negative: 'negative 2', title: 'title 2', control: 'control 2', content: 'content 2', reality: 'reality 2', problems: 'problems 2', name: 'name 2'
+  @concept_aspect1 = create :concept_aspect, discontent_aspect_id: @discontent1.id, concept_post_id: @concept1.id
+  @concept_aspect2 = create :concept_aspect, discontent_aspect_id: @discontent1.id, concept_post_id: @concept2.id
   @condis1 = create :concept_post_discontent, post_id: @concept1.id, discontent_post_id: @discontent1.id
   @condis2 = create :concept_post_discontent, post_id: @concept2.id, discontent_post_id: @discontent1.id
   @comment1 = create :concept_comment, post: @concept1, user: user, content: 'comment 1'
-end
-
-def prepare_plans(project, user)
-  @aspect1 = create :aspect, project: project, content: 'aspect 1'
-  @aspect2 = create :aspect, project: project, content: 'aspect 2'
-  @discontent1 = create :discontent, project: project, status: 4, content: 'discontent 1', whend: 'when 1', whered: 'where 1'
-  @discontent2 = create :discontent, project: project, status: 4, content: 'discontent 2', whend: 'when 2', whered: 'where 2'
-  @disasp1 = create :discontent_post_aspect, post_id: @discontent1.id, aspect_id: @aspect1.id
-  @disasp1 = create :discontent_post_aspect, post_id: @discontent2.id, aspect_id: @aspect1.id
-
-  @concept1 = create :concept, project: project
-  @concept2 = create :concept, project: project
-  @concept_aspect1 = create :concept_aspect, discontent_aspect_id: @discontent1.id, concept_post_id: @concept1.id, positive: 'positive 1', negative: 'negative 1', title: 'title 1', control: 'control 1', content: 'content 1', reality: 'reality 1', problems: 'problems 1', name: 'name 1'
-  @concept_aspect2 = create :concept_aspect, discontent_aspect_id: @discontent1.id, concept_post_id: @concept2.id, positive: 'positive 2', negative: 'negative 2', title: 'title 2', control: 'control 2', content: 'content 2', reality: 'reality 2', problems: 'problems 2', name: 'name 2'
-  @condis1 = create :concept_post_discontent, post_id: @concept1.id, discontent_post_id: @discontent1.id
-  @condis2 = create :concept_post_discontent, post_id: @concept2.id, discontent_post_id: @discontent1.id
-
-  @plan1 = create :plan, project: project, name: 'name 1', goal: 'goal 1', content: 'content 1', user: user
-  @plan_stage1 = create :plan_stage, post_id: @plan1.id, name: 'stage name 1', desc: 'stage desc 1'
-  @plan_aspect1 = create :plan_aspect, plan_post_id: @plan1.id, post_stage_id: @plan_stage1.id, positive: 'concept positive 1', negative: 'concept negative 1', title: 'concept title 1', control: 'control 1', content: 'concept content 1', reality: 'concept reality 1', problems: 'concept problems 1', name: 'concept name 1'
-  @plan_action1 = create :plan_action, plan_post_aspect_id: @plan_aspect1.id, name: 'action name 1', desc: 'action desc 1'
 end
 
 def prepare_estimates(project, user)
@@ -456,8 +435,8 @@ def prepare_estimates(project, user)
 
   @concept1 = create :concept, project: project
   @concept2 = create :concept, project: project
-  @concept_aspect1 = create :concept_aspect, discontent_aspect_id: @discontent1.id, concept_post_id: @concept1.id, positive: 'positive 1', negative: 'negative 1', title: 'title 1', control: 'control 1', content: 'content 1', reality: 'reality 1', problems: 'problems 1', name: 'name 1'
-  @concept_aspect2 = create :concept_aspect, discontent_aspect_id: @discontent1.id, concept_post_id: @concept2.id, positive: 'positive 2', negative: 'negative 2', title: 'title 2', control: 'control 2', content: 'content 2', reality: 'reality 2', problems: 'problems 2', name: 'name 2'
+  @concept_aspect1 = create :concept_aspect, discontent_aspect_id: @discontent1.id, concept_post_id: @concept1.id
+  @concept_aspect2 = create :concept_aspect, discontent_aspect_id: @discontent1.id, concept_post_id: @concept2.id
   @condis1 = create :concept_post_discontent, post_id: @concept1.id, discontent_post_id: @discontent1.id
   @condis2 = create :concept_post_discontent, post_id: @concept2.id, discontent_post_id: @discontent1.id
 
