5f65ca4bb960aabef5da2570771006e1416f5d5a
Denis Karyakin <samhane.me@gmail.com>
2015-01-15 22:43:38 +0300

    Remove factory girl call from utilities

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -201,7 +201,7 @@ def have_content_for_prime_admin(closed_project, opened_project, demo_project, c
 end
 
 def create_invite_for_user(project, user)
-  FactoryGirl.create :core_project_user, project_id: project.id, user_id: user.id
+  create :core_project_user, project_id: project.id, user_id: user.id
 end
 
 def validation_visit_links_for_user(project, user)
@@ -374,98 +374,98 @@ end
 
 def prepare_journal(project, user)
   Journal.destroy_all
-  @journal_today = FactoryGirl.create :journal, project: project, user: user, body: 'news_today', created_at: Time.zone.now.utc.to_date + 12.hours
-  @journal_yesterday = FactoryGirl.create :journal, project: project, user: user, body: 'news_yesterday', created_at: Time.zone.now.utc.yesterday.to_date + 12.hours
-  @journal_older = FactoryGirl.create :journal, project: project, user: user, body: 'news_older', created_at: Time.zone.now.utc.yesterday.to_date - 12.hours
+  @journal_today = create :journal, project: project, user: user, body: 'news_today', created_at: Time.zone.now.utc.to_date + 12.hours
+  @journal_yesterday = create :journal, project: project, user: user, body: 'news_yesterday', created_at: Time.zone.now.utc.yesterday.to_date + 12.hours
+  @journal_older = create :journal, project: project, user: user, body: 'news_older', created_at: Time.zone.now.utc.yesterday.to_date - 12.hours
 end
 
 def prepare_awards
-  FactoryGirl.create :award, name: "1 лайк модератора", url: "1like", position: 1
-  FactoryGirl.create :award, name: "3 лайка модератора", url: "3likes", position: 2
-  FactoryGirl.create :award, name: "5 лайков модератора", url: "5likes", position: 3
-  FactoryGirl.create :award, name: "15 лайков модератора", url: "15likes", position: 4
-  FactoryGirl.create :award, name: "50 лайков модератора", url: "50likes", position: 5
-  FactoryGirl.create :award, name: "Первое несовершенство в аспекте", url: "1stimperfection", position: 6
-  FactoryGirl.create :award, name: "1 несовершенство в аспекте", url: "1imperfection", position: 7
-  FactoryGirl.create :award, name: "3 несовершенства в аспекте", url: "3imperfection", position: 8
-  FactoryGirl.create :award, name: "5 несовершенств в аспекте", url: "5imperfection", position: 9
-  FactoryGirl.create :award, name: "15 и более несовершенств в аспекте", url: "15imperfection", position: 10
-  FactoryGirl.create :award, name: "50 процентов и более несовершенств одного автора в одном аспекте", url: "50imperfection", position: 11
-  FactoryGirl.create :award, name: "Первое нововведение в аспекте", url: "1stinnovation", position: 12
-  FactoryGirl.create :award, name: "1 нововведение в аспекте", url: "1innovation", position: 13
-  FactoryGirl.create :award, name: "3 нововведение в аспекте", url: "3innovation", position: 14
-  FactoryGirl.create :award, name: "5 нововведение в аспекте", url: "5innovation", position: 15
-  FactoryGirl.create :award, name: "15 и более нововведений в аспекте", url: "15innovation", position: 16
-  FactoryGirl.create :award, name: "50 процентов и более нововведений одного автора в одном аспекте", url: "50innovation", position: 17
-  FactoryGirl.create :award, name: "За проект", url: "project", position: 18
-  FactoryGirl.create :award, name: "100 очков рейтинга", url: "100points", position: 19
-  FactoryGirl.create :award, name: "500 очков рейтинга", url: "500points", position: 20
-  FactoryGirl.create :award, name: "1000 очков рейтинга", url: "1000points", position: 21
-  FactoryGirl.create :award, name: "3000 рейтинга и более", url: "3000points", position: 22
+  create :award, name: "1 лайк модератора", url: "1like", position: 1
+  create :award, name: "3 лайка модератора", url: "3likes", position: 2
+  create :award, name: "5 лайков модератора", url: "5likes", position: 3
+  create :award, name: "15 лайков модератора", url: "15likes", position: 4
+  create :award, name: "50 лайков модератора", url: "50likes", position: 5
+  create :award, name: "Первое несовершенство в аспекте", url: "1stimperfection", position: 6
+  create :award, name: "1 несовершенство в аспекте", url: "1imperfection", position: 7
+  create :award, name: "3 несовершенства в аспекте", url: "3imperfection", position: 8
+  create :award, name: "5 несовершенств в аспекте", url: "5imperfection", position: 9
+  create :award, name: "15 и более несовершенств в аспекте", url: "15imperfection", position: 10
+  create :award, name: "50 процентов и более несовершенств одного автора в одном аспекте", url: "50imperfection", position: 11
+  create :award, name: "Первое нововведение в аспекте", url: "1stinnovation", position: 12
+  create :award, name: "1 нововведение в аспекте", url: "1innovation", position: 13
+  create :award, name: "3 нововведение в аспекте", url: "3innovation", position: 14
+  create :award, name: "5 нововведение в аспекте", url: "5innovation", position: 15
+  create :award, name: "15 и более нововведений в аспекте", url: "15innovation", position: 16
+  create :award, name: "50 процентов и более нововведений одного автора в одном аспекте", url: "50innovation", position: 17
+  create :award, name: "За проект", url: "project", position: 18
+  create :award, name: "100 очков рейтинга", url: "100points", position: 19
+  create :award, name: "500 очков рейтинга", url: "500points", position: 20
+  create :award, name: "1000 очков рейтинга", url: "1000points", position: 21
+  create :award, name: "3000 рейтинга и более", url: "3000points", position: 22
 end
 
 def prepare_for_vote_discontents(project)
-  @discontent_group1 = FactoryGirl.create :discontent, project: project, status: 2, content: 'discontent group 1', whend: 'when group 1', whered: 'where group 1'
+  @discontent_group1 = create :discontent, project: project, status: 2, content: 'discontent group 1', whend: 'when group 1', whered: 'where group 1'
 end
 
 def prepare_concepts(project, user)
-  @aspect1 = FactoryGirl.create :aspect, project: project, content: 'aspect 1'
-  @aspect2 = FactoryGirl.create :aspect, project: project, content: 'aspect 2'
-  @discontent1 = FactoryGirl.create :discontent, project: project, status: 4, content: 'discontent 1', whend: 'when 1', whered: 'where 1'
-  @discontent2 = FactoryGirl.create :discontent, project: project, status: 4, content: 'discontent 2', whend: 'when 2', whered: 'where 2'
-  @disasp1 = FactoryGirl.create :discontent_post_aspect, post_id: @discontent1.id, aspect_id: @aspect1.id
-  @disasp1 = FactoryGirl.create :discontent_post_aspect, post_id: @discontent2.id, aspect_id: @aspect1.id
+  @aspect1 = create :aspect, project: project, content: 'aspect 1'
+  @aspect2 = create :aspect, project: project, content: 'aspect 2'
+  @discontent1 = create :discontent, project: project, status: 4, content: 'discontent 1', whend: 'when 1', whered: 'where 1'
+  @discontent2 = create :discontent, project: project, status: 4, content: 'discontent 2', whend: 'when 2', whered: 'where 2'
+  @disasp1 = create :discontent_post_aspect, post_id: @discontent1.id, aspect_id: @aspect1.id
+  @disasp1 = create :discontent_post_aspect, post_id: @discontent2.id, aspect_id: @aspect1.id
 
-  @concept1 = FactoryGirl.create :concept, user: user, project: project
-  @concept2 = FactoryGirl.create :concept, user: user, project: project
-  @concept_aspect1 = FactoryGirl.create :concept_aspect, discontent_aspect_id: @discontent1.id, concept_post_id: @concept1.id, positive: 'positive 1', negative: 'negative 1', title: 'title 1', control: 'control 1', content: 'content 1', reality: 'reality 1', problems: 'problems 1', name: 'name 1'
-  @concept_aspect2 = FactoryGirl.create :concept_aspect, discontent_aspect_id: @discontent1.id, concept_post_id: @concept2.id, positive: 'positive 2', negative: 'negative 2', title: 'title 2', control: 'control 2', content: 'content 2', reality: 'reality 2', problems: 'problems 2', name: 'name 2'
-  @condis1 = FactoryGirl.create :concept_post_discontent, post_id: @concept1.id, discontent_post_id: @discontent1.id
-  @condis2 = FactoryGirl.create :concept_post_discontent, post_id: @concept2.id, discontent_post_id: @discontent1.id
-  @comment1 = FactoryGirl.create :concept_comment, post: @concept1, user: user, content: 'comment 1'
+  @concept1 = create :concept, user: user, project: project
+  @concept2 = create :concept, user: user, project: project
+  @concept_aspect1 = create :concept_aspect, discontent_aspect_id: @discontent1.id, concept_post_id: @concept1.id, positive: 'positive 1', negative: 'negative 1', title: 'title 1', control: 'control 1', content: 'content 1', reality: 'reality 1', problems: 'problems 1', name: 'name 1'
+  @concept_aspect2 = create :concept_aspect, discontent_aspect_id: @discontent1.id, concept_post_id: @concept2.id, positive: 'positive 2', negative: 'negative 2', title: 'title 2', control: 'control 2', content: 'content 2', reality: 'reality 2', problems: 'problems 2', name: 'name 2'
+  @condis1 = create :concept_post_discontent, post_id: @concept1.id, discontent_post_id: @discontent1.id
+  @condis2 = create :concept_post_discontent, post_id: @concept2.id, discontent_post_id: @discontent1.id
+  @comment1 = create :concept_comment, post: @concept1, user: user, content: 'comment 1'
 end
 
 def prepare_plans(project, user)
-  @aspect1 = FactoryGirl.create :aspect, project: project, content: 'aspect 1'
-  @aspect2 = FactoryGirl.create :aspect, project: project, content: 'aspect 2'
-  @discontent1 = FactoryGirl.create :discontent, project: project, status: 4, content: 'discontent 1', whend: 'when 1', whered: 'where 1'
-  @discontent2 = FactoryGirl.create :discontent, project: project, status: 4, content: 'discontent 2', whend: 'when 2', whered: 'where 2'
-  @disasp1 = FactoryGirl.create :discontent_post_aspect, post_id: @discontent1.id, aspect_id: @aspect1.id
-  @disasp1 = FactoryGirl.create :discontent_post_aspect, post_id: @discontent2.id, aspect_id: @aspect1.id
+  @aspect1 = create :aspect, project: project, content: 'aspect 1'
+  @aspect2 = create :aspect, project: project, content: 'aspect 2'
+  @discontent1 = create :discontent, project: project, status: 4, content: 'discontent 1', whend: 'when 1', whered: 'where 1'
+  @discontent2 = create :discontent, project: project, status: 4, content: 'discontent 2', whend: 'when 2', whered: 'where 2'
+  @disasp1 = create :discontent_post_aspect, post_id: @discontent1.id, aspect_id: @aspect1.id
+  @disasp1 = create :discontent_post_aspect, post_id: @discontent2.id, aspect_id: @aspect1.id
 
-  @concept1 = FactoryGirl.create :concept, project: project
-  @concept2 = FactoryGirl.create :concept, project: project
-  @concept_aspect1 = FactoryGirl.create :concept_aspect, discontent_aspect_id: @discontent1.id, concept_post_id: @concept1.id, positive: 'positive 1', negative: 'negative 1', title: 'title 1', control: 'control 1', content: 'content 1', reality: 'reality 1', problems: 'problems 1', name: 'name 1'
-  @concept_aspect2 = FactoryGirl.create :concept_aspect, discontent_aspect_id: @discontent1.id, concept_post_id: @concept2.id, positive: 'positive 2', negative: 'negative 2', title: 'title 2', control: 'control 2', content: 'content 2', reality: 'reality 2', problems: 'problems 2', name: 'name 2'
-  @condis1 = FactoryGirl.create :concept_post_discontent, post_id: @concept1.id, discontent_post_id: @discontent1.id
-  @condis2 = FactoryGirl.create :concept_post_discontent, post_id: @concept2.id, discontent_post_id: @discontent1.id
+  @concept1 = create :concept, project: project
+  @concept2 = create :concept, project: project
+  @concept_aspect1 = create :concept_aspect, discontent_aspect_id: @discontent1.id, concept_post_id: @concept1.id, positive: 'positive 1', negative: 'negative 1', title: 'title 1', control: 'control 1', content: 'content 1', reality: 'reality 1', problems: 'problems 1', name: 'name 1'
+  @concept_aspect2 = create :concept_aspect, discontent_aspect_id: @discontent1.id, concept_post_id: @concept2.id, positive: 'positive 2', negative: 'negative 2', title: 'title 2', control: 'control 2', content: 'content 2', reality: 'reality 2', problems: 'problems 2', name: 'name 2'
+  @condis1 = create :concept_post_discontent, post_id: @concept1.id, discontent_post_id: @discontent1.id
+  @condis2 = create :concept_post_discontent, post_id: @concept2.id, discontent_post_id: @discontent1.id
 
-  @plan1 = FactoryGirl.create :plan, project: project, name: 'name 1', goal: 'goal 1', content: 'content 1', user: user
-  @plan_stage1 = FactoryGirl.create :plan_stage, post_id: @plan1.id, name: 'stage name 1', desc: 'stage desc 1'
-  @plan_aspect1 = FactoryGirl.create :plan_aspect, plan_post_id: @plan1.id, post_stage_id: @plan_stage1.id, positive: 'concept positive 1', negative: 'concept negative 1', title: 'concept title 1', control: 'control 1', content: 'concept content 1', reality: 'concept reality 1', problems: 'concept problems 1', name: 'concept name 1'
-  @plan_action1 = FactoryGirl.create :plan_action, plan_post_aspect_id: @plan_aspect1.id, name: 'action name 1', desc: 'action desc 1'
+  @plan1 = create :plan, project: project, name: 'name 1', goal: 'goal 1', content: 'content 1', user: user
+  @plan_stage1 = create :plan_stage, post_id: @plan1.id, name: 'stage name 1', desc: 'stage desc 1'
+  @plan_aspect1 = create :plan_aspect, plan_post_id: @plan1.id, post_stage_id: @plan_stage1.id, positive: 'concept positive 1', negative: 'concept negative 1', title: 'concept title 1', control: 'control 1', content: 'concept content 1', reality: 'concept reality 1', problems: 'concept problems 1', name: 'concept name 1'
+  @plan_action1 = create :plan_action, plan_post_aspect_id: @plan_aspect1.id, name: 'action name 1', desc: 'action desc 1'
 end
 
 def prepare_estimates(project, user)
-  @aspect1 = FactoryGirl.create :aspect, project: project, content: 'aspect 1'
-  @aspect2 = FactoryGirl.create :aspect, project: project, content: 'aspect 2'
-  @discontent1 = FactoryGirl.create :discontent, project: project, status: 4, content: 'discontent 1', whend: 'when 1', whered: 'where 1'
-  @discontent2 = FactoryGirl.create :discontent, project: project, status: 4, content: 'discontent 2', whend: 'when 2', whered: 'where 2'
-  @disasp1 = FactoryGirl.create :discontent_post_aspect, post_id: @discontent1.id, aspect_id: @aspect1.id
-  @disasp1 = FactoryGirl.create :discontent_post_aspect, post_id: @discontent2.id, aspect_id: @aspect1.id
+  @aspect1 = create :aspect, project: project, content: 'aspect 1'
+  @aspect2 = create :aspect, project: project, content: 'aspect 2'
+  @discontent1 = create :discontent, project: project, status: 4, content: 'discontent 1', whend: 'when 1', whered: 'where 1'
+  @discontent2 = create :discontent, project: project, status: 4, content: 'discontent 2', whend: 'when 2', whered: 'where 2'
+  @disasp1 = create :discontent_post_aspect, post_id: @discontent1.id, aspect_id: @aspect1.id
+  @disasp1 = create :discontent_post_aspect, post_id: @discontent2.id, aspect_id: @aspect1.id
 
-  @concept1 = FactoryGirl.create :concept, project: project
-  @concept2 = FactoryGirl.create :concept, project: project
-  @concept_aspect1 = FactoryGirl.create :concept_aspect, discontent_aspect_id: @discontent1.id, concept_post_id: @concept1.id, positive: 'positive 1', negative: 'negative 1', title: 'title 1', control: 'control 1', content: 'content 1', reality: 'reality 1', problems: 'problems 1', name: 'name 1'
-  @concept_aspect2 = FactoryGirl.create :concept_aspect, discontent_aspect_id: @discontent1.id, concept_post_id: @concept2.id, positive: 'positive 2', negative: 'negative 2', title: 'title 2', control: 'control 2', content: 'content 2', reality: 'reality 2', problems: 'problems 2', name: 'name 2'
-  @condis1 = FactoryGirl.create :concept_post_discontent, post_id: @concept1.id, discontent_post_id: @discontent1.id
-  @condis2 = FactoryGirl.create :concept_post_discontent, post_id: @concept2.id, discontent_post_id: @discontent1.id
+  @concept1 = create :concept, project: project
+  @concept2 = create :concept, project: project
+  @concept_aspect1 = create :concept_aspect, discontent_aspect_id: @discontent1.id, concept_post_id: @concept1.id, positive: 'positive 1', negative: 'negative 1', title: 'title 1', control: 'control 1', content: 'content 1', reality: 'reality 1', problems: 'problems 1', name: 'name 1'
+  @concept_aspect2 = create :concept_aspect, discontent_aspect_id: @discontent1.id, concept_post_id: @concept2.id, positive: 'positive 2', negative: 'negative 2', title: 'title 2', control: 'control 2', content: 'content 2', reality: 'reality 2', problems: 'problems 2', name: 'name 2'
+  @condis1 = create :concept_post_discontent, post_id: @concept1.id, discontent_post_id: @discontent1.id
+  @condis2 = create :concept_post_discontent, post_id: @concept2.id, discontent_post_id: @discontent1.id
 
-  @plan1 = FactoryGirl.create :plan, project: project, name: 'name 1', goal: 'goal 1', content: 'content 1'
-  @plan_stage1 = FactoryGirl.create :plan_stage, post_id: @plan1.id, name: 'stage name 1', desc: 'stage desc 1'
-  @plan_aspect1 = FactoryGirl.create :plan_aspect, plan_post_id: @plan1.id, post_stage_id: @plan_stage1.id, positive: 'concept positive 1', negative: 'concept negative 1', title: 'concept title 1', control: 'control 1', content: 'concept content 1', reality: 'concept reality 1', problems: 'concept problems 1', name: 'concept name 1'
-  @plan_action1 = FactoryGirl.create :plan_action, plan_post_aspect_id: @plan_aspect1.id, name: 'action name 1', desc: 'action desc 1'
+  @plan1 = create :plan, project: project, name: 'name 1', goal: 'goal 1', content: 'content 1'
+  @plan_stage1 = create :plan_stage, post_id: @plan1.id, name: 'stage name 1', desc: 'stage desc 1'
+  @plan_aspect1 = create :plan_aspect, plan_post_id: @plan1.id, post_stage_id: @plan_stage1.id, positive: 'concept positive 1', negative: 'concept negative 1', title: 'concept title 1', control: 'control 1', content: 'concept content 1', reality: 'concept reality 1', problems: 'concept problems 1', name: 'concept name 1'
+  @plan_action1 = create :plan_action, plan_post_aspect_id: @plan_aspect1.id, name: 'action name 1', desc: 'action desc 1'
 
-  @estimate1 = FactoryGirl.create :estimate, project: project, post_id: @plan1.id, user: user, content: 'estimate 1', nepr1: 0, nepr2: 0, nepr3: 0, nepr4: 0, nep1: 0, nep2: 0, nep3: 0, nep4: 0
-  @estimate_aspect1 = FactoryGirl.create :estimate_aspect, post_id: @plan1.id, plan_post_aspect_id: @plan_aspect1.id, op1: 0, op2: 0, op3: 0, op4: 0, on1: 0, on2: 0, on3: 0, on4: 0, ozf1: 0, ozf2: 0, ozf3: 0, ozf4: 0, ozs1: 0, ozs2: 0, ozs3: 0, ozs4: 0
+  @estimate1 = create :estimate, project: project, post_id: @plan1.id, user: user, content: 'estimate 1', nepr1: 0, nepr2: 0, nepr3: 0, nepr4: 0, nep1: 0, nep2: 0, nep3: 0, nep4: 0
+  @estimate_aspect1 = create :estimate_aspect, post_id: @plan1.id, plan_post_aspect_id: @plan_aspect1.id, op1: 0, op2: 0, op3: 0, op4: 0, on1: 0, on2: 0, on3: 0, on4: 0, ozf1: 0, ozf2: 0, ozf3: 0, ozf4: 0, ozs1: 0, ozs2: 0, ozs3: 0, ozs4: 0
 end
