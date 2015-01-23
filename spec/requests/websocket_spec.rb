6b5a4240a9a7c15bb629c4fea19b08a77f6e9ccd
Denis Karyakin <samhane.me@gmail.com>
2015-01-23 15:12:54 +0300

    Update websocket test, now use moderator chat

--- spec/requests/websocket_spec.rb
+++ spec/requests/websocket_spec.rb
@@ -4,13 +4,21 @@ describe 'Users ' do
   subject { page }
 
   context 'ordinary user sign in ', js: true do
-    it 'login' do
+    before do
       visit 'http://0.0.0.0:3000'
       click_link 'sign_in'
-      fill_in 'user_email', with: 'anmalygin@gmail.com'
+      fill_in 'user_email', with: 'test@gmail.com'
       fill_in 'user_password', with: 'pascal2003'
       click_button 'Signin'
-      expect(current_path).to eq root_path
+    end
+
+    it 'go to project' do
+      find("a[id^='go_to_opened_project_']").click
+      #открываем чат
+      find('span.ui-icon-minusthick').click
+      sleep 15
+      find('textarea.ui-chatbox-input-box').set "Test message\n"
+      sleep 15
       screenshot_and_save_page
     end
   end
