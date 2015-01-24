d6eacb9e007d4b46bea8ac772a9dc1de67f47a30
Denis Karyakin <samhane.me@gmail.com>
2015-01-24 19:06:40 +0300

    Refactor and clean up journal spec

--- spec/requests/journal_spec.rb
+++ spec/requests/journal_spec.rb
@@ -1,39 +1,35 @@
-# encoding: utf-8
 require 'spec_helper'
+
 describe 'Journal ' do
   subject { page }
-  # screenshot_and_open_image
-  # save_and_open_page
-  let (:user) {create :user }
-  let (:prime_admin) {create :prime_admin }
-  let (:moderator) {create :moderator }
-  let (:project) {create :core_project, status: 1 }
-  let (:closed_project) {create :core_project, status: 1 , type_access: 2, name: "closed project"}
 
-  let! (:opened_project) { create :core_project, status: 1, type_access: 0, name: "opened project" }
-  let! (:club_project) { create :core_project, status: 1, type_access: 1, name: "club project" }
-  let! (:closed_project) { create :core_project, status: 1, type_access: 2, name: "closed project" }
-  let! (:demo_project) { create :core_project, status: 1, type_access: 3, name: "demo project" }
-  let! (:test_project) { create :core_project, status: 1, type_access: 4, name: "test project" }
+  let (:user) { create :user }
+  let (:moderator) { create :moderator }
+  let (:project) { create :core_project }
+  let! (:opened_project) { create :core_project }
+  let! (:club_project) { create :core_project }
 
-  before  do
-    prepare_journal(project,user)
+  before do
+    Journal.destroy_all
+    @journal_today = create :journal, project: project, user: user, body: 'news_today', created_at: Time.zone.now.utc.to_date + 12.hours
+    @journal_yesterday = create :journal, project: project, user: user, body: 'news_yesterday', created_at: Time.zone.now.utc.yesterday.to_date + 12.hours
+    @journal_older = create :journal, project: project, user: user, body: 'news_older', created_at: Time.zone.now.utc.yesterday.to_date - 12.hours
   end
 
-  context  'ordinary user sign in ' do
+  context 'ordinary user sign in ' do
     before do
       sign_in user
-      visit root_path
     end
+
     context 'success go to project ' do
       before do
         click_link "go_to_opened_project_#{project.id}"
-        click_link "go_to_journals"
+        click_link 'go_to_journals'
       end
 
       it 'have content status ' do
         expect(page).to have_content 'События'
-        expect(page).to have_selector "a", 'вернуться к процедуре'
+        expect(page).to have_selector 'a', 'вернуться к процедуре'
         expect(page).to have_content 'Сегодня'
         expect(page).to have_content 'Вчера'
         expect(page).to have_content 'Ранее'
@@ -45,6 +41,7 @@ describe 'Journal ' do
         expect(page).to have_content 'news_older'
       end
     end
+
     context 'not have go to general_news ' do
       it 'not have news ' do
         expect(page).not_to have_content 'Общие новости'
@@ -56,20 +53,23 @@ describe 'Journal ' do
       end
 
       it 'for general_news ' do
-        visit "/general_news"
+        visit '/general_news'
         expect(page.current_path).to eq root_path
         expect(page).not_to have_content 'Общие новости'
       end
+
       it 'for general_news project ' do
         visit "/project/#{project.id}/general_news"
         expect(page.current_path).to eq root_path
         expect(page).not_to have_content 'Общие новости'
       end
+
       it 'for general_rating ' do
-        visit "/general_rating"
+        visit '/general_rating'
         expect(page.current_path).to eq root_path
         expect(page).not_to have_content 'Общие новости'
       end
+
       it 'for general_rating project ' do
         visit "/project/#{project.id}/general_rating"
         expect(page.current_path).to eq root_path
@@ -81,17 +81,17 @@ describe 'Journal ' do
   context 'moderator sign in ' do
     before do
       sign_in moderator
-      visit root_path
     end
+
     context 'success go to project ' do
       before do
         click_link "go_to_opened_project_#{project.id}"
-        click_link "go_to_journals"
+        click_link 'go_to_journals'
       end
 
       it 'have content status ' do
         expect(page).to have_content 'События'
-        expect(page).to have_selector "a", 'вернуться к процедуре'
+        expect(page).to have_selector 'a', 'вернуться к процедуре'
         expect(page).to have_content 'Сегодня'
         expect(page).to have_content 'Вчера'
         expect(page).to have_content 'Ранее'
@@ -106,7 +106,7 @@ describe 'Journal ' do
 
     context 'success go to general news ' do
       before do
-        click_link "general_news"
+        click_link 'general_news'
       end
 
       it 'have content status and links ' do
@@ -123,9 +123,8 @@ describe 'Journal ' do
       end
 
       it 'have project list ' do
-        expect(page).to have_content "opened project"
-        expect(page).to have_content "club project"
-        expect(page).to have_content "demo project"
+        expect(page).to have_content opened_project.name
+        expect(page).to have_content club_project.name
       end
 
       it 'have filter list ' do
@@ -165,9 +164,10 @@ describe 'Journal ' do
 
     context 'success go to general_rating ' do
       before do
-        click_link "general_news"
-        click_link "go_to_rating"
+        click_link 'general_news'
+        click_link 'go_to_rating'
       end
+
       it 'have content status and links ' do
         expect(page).to have_content 'Рейтинг участников'
         expect(page).to have_content 'ВСЕ УЧАСТНИКИ'
@@ -175,10 +175,5 @@ describe 'Journal ' do
         expect(page).to have_content 'Фильтры'
       end
     end
-
-  end
-
-  context 'expert sign in ' do
-
   end
 end
