93ff2d1c64b9ee43ba8ab4d2988d49cdcd2e2fec
Denis Karyakin <samhane.me@gmail.com>
2015-01-24 19:07:55 +0300

    Clean up knowbase spec

--- spec/requests/knowbase/posts_spec.rb
+++ spec/requests/knowbase/posts_spec.rb
@@ -1,19 +1,18 @@
-# encoding: utf-8
 require 'spec_helper'
+
 describe 'Knowbase ' do
   subject { page }
 
-  let (:user) {create :user }
-  let (:moderator) {create :moderator }
-  let (:project) {create :core_project, status: 1 }
+  let (:user) { create :user }
+  let (:moderator) { create :moderator }
+  let (:project) { create :core_project }
 
   before do
-    @post1 =  FactoryGirl.create :knowbase_post, project: project, title: 'title first knowbase', content: 'content first knowbase', stage: 1
-    @post2 =  FactoryGirl.create :knowbase_post, project: project, title: 'title second knowbase', content: 'content second knowbase', stage: 2
+    @post1 = create :knowbase_post, project: project
+    @post2 = create :knowbase_post, project: project
   end
 
-
-  context  'knowbase post view sign in user ' do
+  context 'knowbase post view sign in user ' do
     before do
       sign_in user
       visit knowbase_posts_path(project)
@@ -32,8 +31,12 @@ describe 'Knowbase ' do
         expect(page).to_not have_selector("li##{@post2.id}.active")
       end
     end
+
     context 'visit knowbase post 2' do
-      before { visit knowbase_post_path(project,@post2) }
+      before do
+        visit knowbase_post_path(project, @post2)
+      end
+
       it ' has all tags' do
         expect(page).to have_selector("span#edit_knowbase_post_title_#{@post2.id}", @post1.title)
         expect(page).to have_selector("div#edit_knowbase_post_content_#{@post2.id}", @post1.content)
@@ -43,7 +46,7 @@ describe 'Knowbase ' do
     end
   end
 
-  context  'knowbase post view sign in admin ' do
+  context 'knowbase post view sign in admin ' do
     before do
       sign_in moderator
       visit knowbase_posts_path(project)
@@ -63,7 +66,9 @@ describe 'Knowbase ' do
       end
 
       context 'edit knowbase post' do
-        before { click_link "edit_knowbase_post_#{@post1.id}" }
+        before do
+          click_link "edit_knowbase_post_#{@post1.id}"
+        end
 
         it 'content and title', js: true do
           expect(page).to have_selector("input#title-textfield", @post1.title)
@@ -72,6 +77,7 @@ describe 'Knowbase ' do
           expect(page).to have_selector("span#edit_knowbase_post_title_#{@post1.id}", 'new title')
         end
       end
+
       context 'delete knowbase post' do
         it 'delete and redirect' do
           expect {
