6b32ab5a62d5c1d759b6dc120597bd4198f9583c
Denis Karyakin <samhane.me@gmail.com>
2015-02-02 23:08:37 +0300

    Show count opened pages and count action in view (one for user and moderator analytic)

--- app/controllers/core/project_users_controller.rb
+++ app/controllers/core/project_users_controller.rb
@@ -7,11 +7,16 @@ class Core::ProjectUsersController < ApplicationController
   def user_analytics
     @count_people = @project.count_people.to_json
     @average_time = @project.average_time.to_json
+    @count_pages = @project.count_pages
+    @count_actions = @project.count_actions('not_moderators')
   end
 
   def moderator_analytics
     @count_people = @project.count_people('for_moderators').to_json
     @average_time = @project.average_time('for_moderators').to_json
+    @count_pages = @project.count_pages('for_moderators')
+    @count_actions = @project.count_actions
+    render action: :user_analytics
   end
 
   private
