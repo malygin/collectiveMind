4d18252508ea99ca15b89310d61d3a22ee08b579
Denis Karyakin <samhane.me@gmail.com>
2015-02-05 22:37:32 +0300

    Refactor users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
-  #before_filter :authenticate, :only: [:edit, :update, :show]
+  before_action :set_user
   before_filter :correct_user, only: [:edit, :update]
-  #before_filter :admin_user, :only: [:destroy]
   before_filter :journal_data, only: [:index, :new, :edit, :show, :users_rc, :journal_clear, :edit_notice]
   before_filter :boss_authenticate, only: [:users_rc]
   before_filter :prime_admin_authenticate, only: [:destroy, :list_users, :add_user_for_project, :remove_user_for_project, :club_toggle, :update_score]
@@ -14,8 +13,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
-    @project = Core::Project.find(params[:project])
     @awards = Award.all
 
     if @user != current_user
@@ -26,8 +23,6 @@ class UsersController < ApplicationController
   end
 
   def journal_clear
-    @project = Core::Project.find(params[:project])
-    @user = User.find(params[:id])
     if @my_journals.size > 0 and current_user?(@user)
       Journal.events_for_my_feed(@project.id, current_user).update_all(viewed: true)
       journal_data
@@ -39,12 +34,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
     add_breadcrumb "Редактирование профиля: #{@user}", edit_user_path(@project, @user)
   end
 
   def index
-    @project = Core::Project.find(params[:project])
     if @project.type_access == 2
       @users = @project.users_in_project.where(users: {type_user: uniq_proc_users}).sort_by { |c| c.core_project_scores.by_project(@project).first.nil? ? 0 : c.core_project_scores.by_project(@project).first.score }.reverse!.uniq
     else
@@ -54,7 +47,6 @@ class UsersController < ApplicationController
 
   def add_user_for_project
     @project = Core::Project.find(params[:project])
-    @user = User.find(params[:id])
     @user.core_project_users.create(project_id: @project.id) unless @user.core_project_users.by_project(@project.id).first
     respond_to do |format|
       format.js
@@ -63,7 +55,6 @@ class UsersController < ApplicationController
 
   def remove_user_for_project
     @project = Core::Project.find(params[:project])
-    @user = User.find(params[:id])
     @user.core_project_users.where(project_id: @project.id).destroy_all
     respond_to do |format|
       format.js
@@ -105,7 +96,6 @@ class UsersController < ApplicationController
 
   def update_score
     @project = Core::Project.find(params[:project])
-    @user = User.find(params[:id])
     params[:value] = '0' if params[:value] == ''
     respond_to do |format|
       if @user.update_attributes(params[:name] => params[:value])
@@ -117,7 +107,6 @@ class UsersController < ApplicationController
 
   def club_toggle
     @project = Core::Project.find(params[:project])
-    @user = User.find(params[:id])
     respond_to do |format|
       if @user.update_attributes!(type_user: club_toggle_user(@user))
         format.js
@@ -127,7 +116,6 @@ class UsersController < ApplicationController
 
   def update
     @project = Core::Project.find(params[:project])
-    @user = User.find(params[:id])
     params[:user].delete(:password) if params[:user][:password].blank?
     if @user.update_attributes(params[:user])
       flash[:success] = 'Профиль обновлен'
@@ -144,12 +132,11 @@ class UsersController < ApplicationController
   end
 
   def add_score
-    user = User.find(params[:id])
     if boss?
       @project = Core::Project.find(params[:project])
-      user.add_score_by_type(@project, params[:score].to_i, :score_a)
-      user.journals.build(type_event: 'add_score', project: @project, body: params[:score]).save
-      current_user.journals.build(type_event: 'my_add_score', user_informed: user, project: @project, body: params[:score], viewed: false).save!
+      @user.add_score_by_type(@project, params[:score].to_i, :score_a)
+      @user.journals.build(type_event: 'add_score', project: @project, body: params[:score]).save
+      current_user.journals.build(type_event: 'my_add_score', user_informed: @user, project: @project, body: params[:score], viewed: false).save!
     end
     respond_to do |format|
       format.js
@@ -157,16 +144,14 @@ class UsersController < ApplicationController
   end
 
   def add_score_essay
-    user = User.find(params[:id])
     if boss?
       @project = Core::Project.find(params[:project])
-      user.journals.build(type_event: 'add_score_essay', project: @project, body: params[:score]).save
+      @user.journals.build(type_event: 'add_score_essay', project: @project, body: params[:score]).save
     end
-    render json: user.score
+    render json: @user.score
   end
 
   def edit_notice
-    @project = Core::Project.find(params[:project])
     @auto_feed_mailer = current_user.user_checks.where(project_id: @project.id, check_field: 'auto_feed_mailer').first
     @journal_mailer = JournalMailer.new
     @mailers = prime_admin? ? @project.journal_mailers : @project.journal_mailers.mailers_for_moderator(current_user)
@@ -186,9 +171,11 @@ class UsersController < ApplicationController
   end
 
   private
+  def set_user
+    @user = User.find(params[:id]) if params[:id]
+  end
 
   def correct_user
-    @user = User.find(params[:id])
     redirect_to(root_path) unless current_user?(@user)
   end
 end
