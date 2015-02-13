cb42d7f1346427f8c867064e88cb5b2a292a2623
irivertime <irivertime@mail.ru>
2015-02-14 00:44:37 +0300

    fix tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,6 +43,7 @@ Spork.prefork do
 
     config.before :each do
       I18n.default_locale = :en
+      I18n.locale = :en
     end
 
     config.before :each, js: true do
@@ -50,6 +51,8 @@ Spork.prefork do
       page.driver.allow_url '0.0.0.0'
       page.driver.allow_url 'res.cloudinary.com'
       Capybara.default_wait_time = 5
+      I18n.default_locale = :en
+      I18n.locale = :en
     end
     config.before(:suite) do
       DatabaseCleaner.clean_with(:truncation)
