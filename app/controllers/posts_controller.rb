65a75064e24e6360f3481ced5f0b7a76c467fa73
Denis Karyakin <samhane.me@gmail.com>
2015-02-16 15:45:01 +0300

    Delete unused filters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,6 @@
 class PostsController < ProjectsController
-  before_filter :authenticate_user!
-  before_filter :prepare_data, only: [:index, :new, :edit, :show, :vote_list, :vote_result, :to_work, :about]
   before_filter :journal_data, only: [:index, :new, :edit, :show, :vote_list, :vote_result, :to_work, :about]
-  before_filter :have_rights, only: [:edit]
   before_filter :have_project_access
-  before_filter :not_open_closed_stage
   before_filter :boss_authenticate, only: [:vote_result]
   before_filter :comment_page, only: [:index, :show]
 
