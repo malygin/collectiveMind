8a2bfe58a706e762cf02d1f991a3b177b330805f
Denis Karyakin <samhane.me@gmail.com>
2015-02-17 14:20:03 +0300

    Commented execute by rename pkeys in migrations, update schema

--- db/migrate/20150215212130_rename_for_collect_info.rb
+++ db/migrate/20150215212130_rename_for_collect_info.rb
@@ -3,23 +3,23 @@ class RenameForCollectInfo < ActiveRecord::Migration
     remove_column :core_aspects, :user_add
 
     rename_column :collect_info_votings, :discontent_aspect_id, :aspect_id
-    execute "ALTER TABLE collect_info_votings DROP CONSTRAINT life_tape_voitings_pkey;"
-    execute "ALTER TABLE collect_info_votings ADD PRIMARY KEY (id);"
+    #execute "ALTER TABLE collect_info_votings DROP CONSTRAINT life_tape_voitings_pkey;"
+    #execute "ALTER TABLE collect_info_votings ADD PRIMARY KEY (id);"
 
     remove_column :collect_info_questions, :raiting
     remove_column :collect_info_questions, :post_id
     remove_column :collect_info_questions, :parent_post_type
     add_column :collect_info_questions, :aspect_id, :integer
-    execute "ALTER TABLE collect_info_questions DROP CONSTRAINT questions_pkey;"
-    execute "ALTER TABLE collect_info_questions ADD PRIMARY KEY (id);"
+    #execute "ALTER TABLE collect_info_questions DROP CONSTRAINT questions_pkey;"
+    #execute "ALTER TABLE collect_info_questions ADD PRIMARY KEY (id);"
 
 
     remove_column :collect_info_answers, :raiting
     remove_column :collect_info_answers, :style
     add_column :collect_info_answers, :correct, :boolean
 
-    execute "ALTER TABLE collect_info_answers DROP CONSTRAINT answers_pkey;"
-    execute "ALTER TABLE collect_info_answers ADD PRIMARY KEY (id);"
+    #execute "ALTER TABLE collect_info_answers DROP CONSTRAINT answers_pkey;"
+    #execute "ALTER TABLE collect_info_answers ADD PRIMARY KEY (id);"
 
     drop_table :collect_info_answers_users
 
