70a0a785b8cf2f9cb9a64491af6bce441edcf4b5
irivertime <irivertime@mail.ru>
2015-03-06 15:59:50 +0300

    fix answers

--- app/controllers/core/aspects_controller.rb
+++ app/controllers/core/aspects_controller.rb
@@ -29,26 +29,33 @@ class Core::AspectsController < ProjectsController
     @aspect = params[:id] ? Core::Aspect.find(params[:id]) : @project.aspects.order(:id).first
     @question = CollectInfo::Question.find(params[:question_id])
     aspect_questions = @aspect.questions.by_project(@project).by_status(0).order("collect_info_questions.id")
-    @answers = @question.answers.by_status(0).by_uncorrect.pluck("collect_info_answers.id")
+    @uncorrect_answers = @question.answers.by_status(0).by_uncorrect.pluck("collect_info_answers.id")
+    @correct_answers = @question.answers.by_status(0).by_correct.pluck("collect_info_answers.id")
     if params[:answers]
-      params[:answers].each do |answer|
-        @wrong_answer = true if @answers.include? answer.to_i
-      end
-    end
-    unless @wrong_answer
-      if params[:answers]
-        params[:answers].each do |answer|
-          current_user.user_answers.create(answer_id: answer.to_i, project_id: @project.id, question_id: @question.id)
+      # params[:answers].each do |answer|
+      #   @wrong_answer = true if @uncorrect_answers.include? answer.to_i
+      # end
+      # params[:answers].each do |answer|
+      #   @wrong_answer = true unless @correct_answers.include? answer.to_i
+      # end
+      arr = @correct_answers - params[:answers].collect{|a| a.to_i}
+      @wrong_answer = true if arr.present?
+
+      unless @wrong_answer
+        if params[:answers]
+          params[:answers].each do |answer|
+            current_user.user_answers.create(answer_id: answer.to_i, project_id: @project.id, question_id: @question.id)
+          end
         end
-      end
-      @next_question = aspect_questions[(aspect_questions.index @question) + 1]
-      @count_now = @aspect.question_complete(@project, current_user).count
-      @count_all = @aspect.questions.by_project(@project).by_status(0).count
-      unless @next_question
-        @count_aspects = @project.main_aspects.count
-        @count_aspects_check = 0
-        @project.main_aspects.each do |asp|
-          @count_aspects_check += 1 if asp.question_complete(@project, current_user).count == asp.questions.by_project(@project).by_status(0).count
+        @next_question = aspect_questions[(aspect_questions.index @question) + 1]
+        @count_now = @aspect.question_complete(@project, current_user).count
+        @count_all = @aspect.questions.by_project(@project).by_status(0).count
+        unless @next_question
+          @count_aspects = @project.main_aspects.count
+          @count_aspects_check = 0
+          @project.main_aspects.each do |asp|
+            @count_aspects_check += 1 if asp.question_complete(@project, current_user).count == asp.questions.by_project(@project).by_status(0).count
+          end
         end
       end
     end
