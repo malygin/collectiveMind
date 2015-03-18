76fc71d61bec589924192b9bbea485d5ae79a0b2
Denis Karyakin <samhane.me@gmail.com>
2015-03-18 21:37:02 +0300

    Try to run pubnub

--- config/initializers/pubnub.rb
+++ config/initializers/pubnub.rb
@@ -1,4 +1,4 @@
 $pubnub = Pubnub.new(
-    publish_key: 'demo',
-    subscribe_key: 'demo'
+    publish_key: ENV['PUBNUB_SUBSCRIBE_KEY'].to_s || 'demo',
+    subscribe_key: ENV['PUBNUB_PUBLISH_KEY'].to_s || 'demo'
 )
