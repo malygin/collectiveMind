dadc99db39f49070c83537b00460449396b1f249
Denis Karyakin <samhane.me@gmail.com>
2015-03-18 18:01:28 +0300

    Replace pubnub js and set demo credentials

--- config/initializers/pubnub.rb
+++ config/initializers/pubnub.rb
@@ -1,4 +1,4 @@
 $pubnub = Pubnub.new(
-    publish_key: ENV['PUBNUB_SUBSCRIBE_KEY'] || 'demo',
-    subscribe_key: ENV['PUBNUB_PUBLISH_KEY'] || 'demo'
+    publish_key: 'demo',
+    subscribe_key: 'demo'
 )
