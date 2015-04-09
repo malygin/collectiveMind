32b879572e405fe52b4ac86a0668d7e57fa02f7b
Denis Karyakin <samhane.me@gmail.com>
2015-04-09 18:56:41 +0300

    Fix data migration for type procedure

--- db/data/20150408114901_add_types_procedure.rb
+++ db/data/20150408114901_add_types_procedure.rb
@@ -1,9 +1,10 @@
 class AddTypesProcedure < SeedMigration::Migration
   def up
-    Core::ProjectType.create name: 'Бизнес', custom_fields: {concept_posts: [:reality]}
+    Core::ProjectType.create name: 'Бизнес', code: 'business'
   end
 
   def down
     Core::ProjectType.find_by(name: 'Бизнес').destroy
+    ActiveRecord::Base.connection.reset_pk_sequence! 'core_project_types'
   end
 end
