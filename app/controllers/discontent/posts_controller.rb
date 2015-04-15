58ea8703aaf203781aa2d570ce8ecc9448fc9100
Denis Karyakin <samhane.me@gmail.com>
2015-04-15 17:49:54 +0300

    Fix discontent controller

--- app/controllers/discontent/posts_controller.rb
+++ app/controllers/discontent/posts_controller.rb
@@ -2,6 +2,8 @@ class Discontent::PostsController < PostsController
   include Discontent::PostsHelper
   include DiscontentGroup
 
+  before_action :set_aspects, only: [:index, :new]
+
   def voting_model
     Discontent::Post
   end
@@ -18,7 +20,7 @@ class Discontent::PostsController < PostsController
 
   def index
     @posts= nil
-    if  params[:aspect]
+    if params[:aspect]
       @posts = Core::Aspect.find(params[:aspect].scan(/\d/).join('')).aspect_posts
     else
       @posts = @project.discontents.by_status_for_discontent(@project)
@@ -26,11 +28,11 @@ class Discontent::PostsController < PostsController
     respond_to do |format|
 
       format.html # show.html.erb
-      format.json { render json: @posts.map {|item| {id: item.id, content: item.content, whend: item.whend, whered: item.whered,
-                                                     user:item.user.to_s, post_date: Russian::strftime(item.created_at,'%d.%m.%Y %k:%M:%S'),
-                                                     project_id: item.project_id, sort_date: item.created_at.to_datetime.to_f, sort_rate: (item.users_pro.count + (item.number_views * 0.3)),
-                                                     aspect_class: post_aspect_classes(item),
-                                                     aspects: item.post_aspects.map {|aspect|{id: aspect.id, color: aspect.color, content: aspect.content} } }  } }
+      format.json { render json: @posts.map { |item| {id: item.id, content: item.content, whend: item.whend, whered: item.whered,
+                                                      user: item.user.to_s, post_date: Russian::strftime(item.created_at, '%d.%m.%Y %k:%M:%S'),
+                                                      project_id: item.project_id, sort_date: item.created_at.to_datetime.to_f, sort_rate: (item.users_pro.count + (item.number_views * 0.3)),
+                                                      aspect_class: post_aspect_classes(item),
+                                                      aspects: item.post_aspects.map { |aspect| {id: aspect.id, color: aspect.color, content: aspect.content} }} } }
 
     end
   end
