5e240eade95fff6486f775324dc7e9a3c1d97542
Denis Karyakin <samhane.me@gmail.com>
2015-04-17 18:17:03 +0300

    Fix model spec for answers

--- spec/models/collect_info/answer_spec.rb
+++ spec/models/collect_info/answer_spec.rb
@@ -13,9 +13,5 @@ describe 'CollectInfo::Answer', type: :model do
     it 'project' do
       expect(build(:collect_info_answer, question: nil)).to be_invalid
     end
-
-    it 'correct' do
-      expect(build(:collect_info_answer, correct: nil)).to be_invalid
-    end
   end
 end
