bbdfc627454ecfacbd5546cc3609e18698999a1a
irivertime <irivertime@mail.ru>
2015-05-05 21:28:11 +0300

    fix novations and gliphs

--- app/controllers/novation/posts_controller.rb
+++ app/controllers/novation/posts_controller.rb
@@ -10,7 +10,7 @@ class Novation::PostsController < PostsController
     respond_to do |format|
 
       format.html # show.html.erb
-      format.json { render json: @posts.each_with_index.map { |item, index| {id: item.id, index: index+1, title: item.title, content: trim_post_content(item.project_change, 200),
+      format.json { render json: @posts.each_with_index.map { |item, index| {id: item.id, index: index+1, title: item.title.present? ? item.title : 'Пакет без названия', content: trim_post_content(item.project_change, 200),
                                                                              user: item.user.to_s, user_avatar: item.user.try(:avatar) ? cl_image_path(item.user.try(:avatar)) : ActionController::Base.helpers.asset_path('no-ava.png'), post_date: Russian::strftime(item.created_at, '%d.%m.%Y'),
                                                                              project_id: item.project_id, sort_date: item.created_at.to_datetime.to_f, sort_comment: item.last_comment.present? ? item.last_comment.created_at.to_datetime.to_f : 0,
                                                                              concept_class: post_concept_classes(item),
