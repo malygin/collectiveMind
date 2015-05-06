496e023e8ec2359559d925d81cce9b5b28ec7d52
Denis Karyakin <samhane.me@gmail.com>
2015-05-06 11:25:18 +0300

    Rename plan post_aspect to post_novation, add translations to post_novation attributes, add needed fields and order

--- app/models/plan/post_novation.rb
+++ app/models/plan/post_novation.rb
@@ -1,7 +1,36 @@
-class Plan::PostAspect < ActiveRecord::Base
+class Plan::PostNovation < ActiveRecord::Base
   belongs_to :novation_post, class_name: 'Novation::Post'
   belongs_to :plan_post, class_name: 'Plan::Post', foreign_key: :plan_post_id
 
   has_many :plan_post_actions, -> { order :date_begin }, class_name: 'Plan::PostAction', foreign_key: :plan_post_aspect_id
 
+  def attributes_for_form
+    {project: {
+        project_goal: -1,
+        project_change: -1,
+        project_members: -1,
+        project_results: -1,
+        project_time: -1,
+        members_execute: -1,
+        members_who: -1,
+        resource_financial: -1,
+        resource_internal: -1,
+        resource_external: true,
+        resource_commands: -1,
+    },
+     members: {
+         members_new: true,
+         members_motivation: true,
+         members_education: true,
+         confidence_commands: false,
+         resource_support: false,
+     },
+     resource: {
+         resource_competition: true,
+     },
+     confidence: {
+         confidence_remove_discontent: false,
+         confidence_negative_results: true
+     }}
+  end
 end
