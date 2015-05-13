02540becd93d9911e2ed9f1fade913df911de326
Denis Karyakin <samhane.me@gmail.com>
2015-05-13 16:11:25 +0300

    Remove Plan::PostAction, Plan::PostStage

--- app/models/plan/post_stage.rb
+++
@@ -1,9 +0,0 @@
-class Plan::PostStage < ActiveRecord::Base
-  belongs_to :post, class_name: 'Plan::Post'
-  # has_many :plan_post_aspects, class_name: 'Plan::PostAspect', foreign_key: :post_stage_id
-
-  def actions_rowcount
-    self.plan_post_aspects.
-        joins('INNER JOIN "plan_post_actions" ON "plan_post_aspects"."id" = "plan_post_actions"."plan_post_aspect_id"')
-  end
-end
