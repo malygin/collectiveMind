a53867d05ef1200806f7d12a205d2596fa923610
Denis Karyakin <samhane.me@gmail.com>
2015-05-15 01:42:49 +0300

    Fix concepts specs, without create with discontents

--- spec/requests/cabinet/concepts_spec.rb
+++ spec/requests/cabinet/concepts_spec.rb
@@ -14,12 +14,12 @@ describe 'Cabinet Concepts' do
 
   it_behaves_like 'base cabinet'
 
-  describe 'create with simple form', js: true, skip: true do
+  describe 'create with simple form', js: true do
     before do
       click_link 'new_concept_posts_simple'
     end
 
-    context 'correct' do
+    context 'correct', skip: true do
       before do
         fill_in 'concept_post_title', with: 'title for concept'
         fill_in 'concept_post_goal', with: 'goal in concept'
@@ -27,8 +27,7 @@ describe 'Cabinet Concepts' do
         fill_in 'concept_post_actors', with: 'actors'
         fill_in 'concept_post_impact_env', with: 'impact environment'
         find('#open_discontents').click
-
-        find("input#concept_post_discontents_#{discontent.id}").click
+        find("input#concept_post_discontents_#{@discontent.id}").click
         click_button 'send_post_concept'
       end
 
@@ -43,46 +42,58 @@ describe 'Cabinet Concepts' do
       expect {
         click_button 'send_post_concept'
         within :css, 'div.notice_messages' do
-          expect(page).to have_css 'div.error_explanation'
+          expect(page).to have_css 'div#error_explanation'
         end
       }.not_to change(Concept::Post, :count)
     end
   end
 
-  xit 'edit', js: true do
+  it 'edit', js: true do
     new_content = 'new cool content'
-    visit user_content_concept_posts_path(project)
-    click_link "edit_concept_#{concept.id}"
+    visit user_content_concept_posts_path(@project)
+    click_link "edit_concept_#{@concept.id}"
     within :css, '.checked_items' do
-      concept.concept_disposts.each do |discontent|
+      @concept.concept_disposts.each do |discontent|
         expect(page).to have_content discontent.content
       end
     end
     within :css, '.unchecked_items' do
-      expect(page).to have_content discontent.content
+      expect(page).to have_content @discontent.content
     end
     expect {
-      fill_in 'concept_post_content', with: new_content
-      click_button 'send_post'
+      fill_in 'concept_post_title', with: new_content
+      click_button 'send_post_concept'
       expect(page).to have_content t('form.concept.edit_success')
     }.not_to change(Concept::Post, :count)
-    visit user_content_concept_posts_path(project)
+    visit user_content_concept_posts_path(@project)
     expect(page).to have_content new_content
   end
 
   context 'destroy' do
     it 'author - ok', js: true do
-      visit user_content_concept_posts_path(project)
+      visit user_content_concept_posts_path(@project)
       expect {
-        click_link "destroy_concept_#{concept.id}"
+        click_link "destroy_concept_#{@concept.id}"
         page.driver.browser.accept_js_confirms
-        expect(current_path) == user_content_concept_posts_path(project)
+        expect(current_path) == user_content_concept_posts_path(@project)
       }.to change(Concept::Post, :count).by(-1)
     end
   end
 
   it 'created by current user' do
     click_link 'open_my_concept_posts'
-    expect(page).to have_content concept.content
+    expect(page).to have_content @concept.title
+    @concept.concept_disposts.each do |discontent|
+      expect(page).to have_content discontent.content
+    end
+  end
+
+  it 'publish', js: true do
+    visit edit_concept_post_path(@project, @concept)
+    expect {
+      click_link "publish_#{@concept.id}"
+      refresh_page
+      expect(page).not_to have_link "publish_#{@concept.id}"
+    }.to change(Concept::Post.published, :count).by(1)
   end
 end
