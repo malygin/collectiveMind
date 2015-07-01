ffb3cc4a5c89e2326cdeeb288bc8ccbc93f650fe
irivertime <irivertime@mail.ru>
2015-07-01 17:35:13 +0300

    show button for add content on stages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -162,7 +162,9 @@ class PostsController < ProjectsController
   def check_stage_for_cabinet
     #  check if user reload url for old-stage cabinet, and procedure on new stage - redirect to new stage
     return if @project.current_stage_type == params[:controller].sub('/', '_').to_sym ||
-              action_name == 'user_content' || @project.current_stage_type == :collect_info_posts && params[:controller].sub('/', '_').to_sym == :'core_aspect/posts'
+              action_name == 'user_content' ||
+              (@project.current_stage_type == :collect_info_posts && params[:controller].sub('/', '_').to_sym == :'core_aspect/posts') ||
+              @project.status == 100
     redirect_to url_for(params.merge(controller: '/' + @project.current_stage_type.to_s.sub('_', '/')))
   end
 
